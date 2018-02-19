c1bae4210aa937d778251ef1c695333014906cc6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-02-19 00:50:28 -0800

    vacuum tables after copy

--- src/data/create-database.bash
+++ src/data/create-database.bash
@@ -37,7 +37,10 @@ do
     sed "s;ENDDATE;${tabledate};" ${tablename}.ddl | psql
 
     # run the copy
-    /usr/bin/time psql -c "\copy ${db_tablename} from '${interim}/${filename}' with csv header"
+    export copy_command="\copy ${db_tablename} from '${interim}/${filename}' with csv header"
+    echo "${copy_command}"
+    /usr/bin/time psql -c "${copy_command}"
+    psql -c "VACUUM ANALYZE ${db_tablename};"
 
     # dump the table
     /usr/bin/time pg_dump --format=custom --no-owner --clean --if-exists --table=${db_tablename} \
