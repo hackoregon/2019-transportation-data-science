79a3e24b87057f7ca402cbfb8da7ddbca3d8a519
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-02-22 23:34:45 -0800

    more cleanup

--- src/data/3backup-database.bash
+++ src/data/3backup-database.bash
@@ -3,11 +3,7 @@
 # define parameters
 export interim="../../data/interim"
 export PGDATABASE=trimet_congestion
-export backups=${interim}/backups
 export JOBS=7 # one less than your processor / thread count - a four-core / eight-thread would use seven
 
-# rclone requires a directory for bulk uploads
-mkdir -p ${backups}
-
 /usr/bin/time pg_dump --format=directory --jobs=${JOBS} \
-  --file=${backups}/trimet_congestion.backup $PGDATABASE
+  --file=${interim}/trimet_congestion.backup $PGDATABASE
