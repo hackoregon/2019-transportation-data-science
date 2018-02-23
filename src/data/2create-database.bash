c039704b3d124ac838f97dcd3e2b7f9b9f9febb3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-02-22 22:10:19 -0800

    cleanup

--- src/data/2create-database.bash
+++ src/data/2create-database.bash
@@ -14,8 +14,7 @@ for tablename in init_cyclic_v1h init_veh_stoph trimet_stop_event init_tripsh
 do
   psql -f "${tablename}.ddl"
   export copy_command="\copy ${tablename} from '${interim}/${tablename}.csv' with csv"
-  echo ${copy_command}
-  /usr/bin/time psql -c "${copy_command}" 2>&1 | tee ${tablename}.log &
+  /usr/bin/time psql -c "${copy_command}" &
 done
 
 # measure size after we load data
