767744a7af68df39762de61583e4a0fb5136e466
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-02-22 21:57:22 -0800

    more cleanup

--- src/data/3backup-database.bash
+++ src/data/3backup-database.bash
@@ -1,10 +1,6 @@
 #! /bin/bash
 
 # define parameters
-export filedates=("1-30SEP2017" "1-31OCT2017" "1-30NOV2017")
-export tabledates=("ended_20170930" "ended_20171031" "ended_20171130")
-export tablenames=("init_cyclic_v1h" "init_veh_stoph" "trimet_stop_event" "init_tripsh")
-export raw="../../data/raw"
 export interim="../../data/interim"
 export PGDATABASE=trimet_congestion
 export backups=${interim}/backups
@@ -13,5 +9,5 @@ export JOBS=7 # one less than your processor / thread count - a four-core / eigh
 # rclone requires a directory for bulk uploads
 mkdir -p ${backups}
 
-/usr/bin/time pg_dump --format=directory --jobs=${JOBS} --no-owner --clean --if-exists \
+/usr/bin/time pg_dump --format=directory --jobs=${JOBS} \
   --file=${backups}/trimet_congestion.backup $PGDATABASE
