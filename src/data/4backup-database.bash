e0b14416f121aff2abb63dfc9fed4fbe1198ee56
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-02 23:02:57 -0700

    use a 9.6 version of pg_dump

--- src/data/4backup-database.bash
+++ src/data/4backup-database.bash
@@ -11,5 +11,7 @@ export interim="../../data/interim"
 export PGDATABASE=trimet_congestion
 export JOBS=7 # one less than your processor / thread count - a four-core / eight-thread would use seven
 
+# using 9.6 dump program!
+export PATH=/d/opt/PostgreSQL/9.6/bin/:$PATH
 time pg_dump --format=directory --jobs=${JOBS} \
   --file=${interim}/trimet_congestion.backup ${PGDATABASE}
