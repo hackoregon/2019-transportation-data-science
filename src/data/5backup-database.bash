3dd5d1b6ebe1618ed401e98bfe462ff5cf699749
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-03 13:25:39 -0700

    remove old backup first

--- src/data/5backup-database.bash
+++ src/data/5backup-database.bash
@@ -9,6 +9,8 @@ export PGDATABASE=trimet_congestion
 export JOBS=7 # one less than your processor / thread count - a four-core / eight-thread would use seven
 
 # using 9.6 dump program!
-export PATH=/d/opt/PostgreSQL/9.6/bin/:$PATH
+echo "Removing previous backup directory if any"
+rm -fr $interim}/trimet_congestion.backup
+echo "Making a backup"
 pg_dump --format=directory --jobs=${JOBS} \
   --file=${interim}/trimet_congestion.backup ${PGDATABASE}
