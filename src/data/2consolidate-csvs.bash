5ce0e4566f8cb2c5e211a80d9a33feaace798380
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-03 00:05:20 -0700

    useless cats removed

--- src/data/2consolidate-csvs.bash
+++ src/data/2consolidate-csvs.bash
@@ -6,26 +6,26 @@ export raw="../raw"
 
 echo "Consolidating the CSVs"
 pushd ${interim}
-cat \
+grep -v VEHICLE_ID \
   "${raw}/init_cyclic_v1h 1-30SEP2017.csv" \
   "${raw}/init_cyclic_v1h 1-31OCT2017.csv" \
   "${raw}/init_cyclic_v1h 1-30NOV2017.csv" \
-  | grep -v VEHICLE_ID > init_cyclic_v1h.csv &
-cat \
+  > init_cyclic_v1h.csv &
+grep -v VEHICLE_ID \
   "${raw}/init_veh_stoph 1-30SEP2017.csv" \
   "${raw}/init_veh_stoph 1-31OCT2017.csv" \
   "${raw}/init_veh_stoph 1-30NOV2017.csv" \
-  | grep -v VEHICLE_ID > init_veh_stoph.csv &
-cat \
+  > init_veh_stoph.csv &
+grep -v VEHICLE_NUMBER \
   "${raw}/trimet_stop_event 1-30SEP2017.csv" \
   "${raw}/trimet_stop_event 1-31OCT2017.csv" \
   "${raw}/trimet_stop_event 1-30NOV2017.csv" \
-  | grep -v VEHICLE_NUMBER > trimet_stop_event.csv &
-cat \
+  > trimet_stop_event.csv &
+grep -v VEHICLE_ID \
   "${raw}/init_tripsh 1-30SEP2017.csv" \
   "${raw}/init_tripsh 1-31OCT2017.csv" \
   "${raw}/init_tripsh 1-30NOV2017.csv" \
-  | grep -v VEHICLE_ID > init_tripsh.csv &
+  > init_tripsh.csv &
 
 wait
 
