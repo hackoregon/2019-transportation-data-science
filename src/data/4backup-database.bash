c54ca73f5fc7e1cece5d3ffcf951cc36172d8d83
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-03 17:46:31 -0700

    typo

--- src/data/4backup-database.bash
+++ src/data/4backup-database.bash
@@ -10,7 +10,7 @@ export JOBS=7 # one less than your processor / thread count - a four-core / eigh
 
 # using 9.6 dump program!
 echo "Removing previous backup directory if any"
-rm -fr $interim}/trimet_congestion.backup
+rm -fr ${interim}/trimet_congestion.backup
 echo "Making a backup"
 pg_dump --format=directory --jobs=${JOBS} \
   --file=${interim}/trimet_congestion.backup ${PGDATABASE}
