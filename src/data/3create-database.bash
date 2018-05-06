3056e50961bd6c6414726c2db512d891b1ab0c10
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-06 00:09:46 -0700

    do backup after create

--- src/data/3create-database.bash
+++ src/data/3create-database.bash
@@ -22,10 +22,19 @@ do
   psql -f "${tablename}.ddl"
   export copy_command="\copy ${tablename} from '${interim}/${tablename}.csv' with csv"
   echo "Starting background load of table ${tablename}"
-  psql -c "${copy_command}" &
+  /usr/bin/time psql -c "${copy_command}" &
 done
 
 echo "Waiting for table loads"
 wait
 echo "VACUUM ANALYZE"
-psql -d ${PGDATABASE} -c "VACUUM ANALYZE;"
+/usr/bin/time psql -d ${PGDATABASE} -c "VACUUM ANALYZE;"
+
+export JOBS=7 # one less than your processor / thread count - a four-core / eight-thread would use seven
+
+echo "Removing previous backup directory if any"
+rm -fr ${interim}/trimet_congestion.backup
+echo "Making a backup"
+/usr/bin/time pg_dump --format=directory --jobs=${JOBS} \
+  --file=${interim}/trimet_congestion.backup ${PGDATABASE}
+pg_restore --list ${interim}/trimet_congestion.backup
