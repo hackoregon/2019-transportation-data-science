a7173c409c1f404c03b780427914a098486d8452
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-07 16:23:06 -0700

    typo

--- src/data/2bconsolidate-init_veh_stoph.bash
+++ src/data/2bconsolidate-init_veh_stoph.bash
@@ -6,7 +6,7 @@ export raw="../raw"
 
 echo "Consolidating the CSVs"
 pushd ${interim}
-head -n 1 "${raw}/init_veh_stop 1-30SEP2017.csv" | tr '[:upper:]' '[:lower:]' > init_veh_stop.csv
+head -n 1 "${raw}/init_veh_stoph 1-30SEP2017.csv" | tr '[:upper:]' '[:lower:]' > init_veh_stoph.csv
 grep -h -v VEHICLE_ID \
   "${raw}/init_veh_stoph 1-30SEP2017.csv" \
   "${raw}/init_veh_stoph 1-31OCT2017.csv" \
