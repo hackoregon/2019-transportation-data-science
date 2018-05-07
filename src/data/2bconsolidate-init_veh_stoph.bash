b42a34bca3ef8777889e000b84fe4bcf2894c647
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-07 16:06:07 -0700

    put the headers back

--- src/data/2bconsolidate-init_veh_stoph.bash
+++ src/data/2bconsolidate-init_veh_stoph.bash
@@ -6,9 +6,10 @@ export raw="../raw"
 
 echo "Consolidating the CSVs"
 pushd ${interim}
+head -n 1 "${raw}/init_veh_stop 1-30SEP2017.csv" | tr '[:upper:]' '[:lower:]' > init_veh_stop.csv
 grep -h -v VEHICLE_ID \
   "${raw}/init_veh_stoph 1-30SEP2017.csv" \
   "${raw}/init_veh_stoph 1-31OCT2017.csv" \
   "${raw}/init_veh_stoph 1-30NOV2017.csv" \
-  > init_veh_stoph.csv
+  >> init_veh_stoph.csv
 popd
--- src/data/2cconsolidate-trimet_stop_event.bash
+++ src/data/2cconsolidate-trimet_stop_event.bash
@@ -6,9 +6,10 @@ export raw="../raw"
 
 echo "Consolidating the CSVs"
 pushd ${interim}
+head -n 1 "${raw}/trimet_stop_event 1-30SEP2017.csv" | tr '[:upper:]' '[:lower:]' > trimet_stop_event.csv
 grep -h -v VEHICLE_NUMBER \
   "${raw}/trimet_stop_event 1-30SEP2017.csv" \
   "${raw}/trimet_stop_event 1-31OCT2017.csv" \
   "${raw}/trimet_stop_event 1-30NOV2017.csv" \
-  > trimet_stop_event.csv
+  >> trimet_stop_event.csv
 popd
