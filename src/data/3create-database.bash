4001d23f144c3e8f860083b7683b51f7404e68a4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-07 20:36:40 -0700

    don't back up database yet

--- src/data/3create-database.bash
+++ src/data/3create-database.bash
@@ -29,12 +29,3 @@ echo "Waiting for table loads"
 wait
 echo "VACUUM ANALYZE"
 /usr/bin/time psql -d ${PGDATABASE} -c "VACUUM ANALYZE;"
-
-export JOBS=7 # one less than your processor / thread count - a four-core / eight-thread would use seven
-
-echo "Removing previous backup directory if any"
-rm -fr ${interim}/trimet_congestion.backup
-echo "Making a backup"
-/usr/bin/time pg_dump --format=directory --jobs=${JOBS} \
-  --file=${interim}/trimet_congestion.backup ${PGDATABASE}
-pg_restore --list ${interim}/trimet_congestion.backup
