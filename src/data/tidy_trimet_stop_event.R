91d594e1ed25ec25ba5bee7a14d5e9c44ea49705
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-08 13:56:28 -0700

    script to do the final conversions

---
+++ src/data/tidy_trimet_stop_event.R
@@ -0,0 +1,37 @@
+if (!require(tidyverse)) install.packages("tidyverse")
+library(tidyverse)
+library(magrittr)
+
+trimet_stop_event <- read_csv(
+  "../../data/interim/trimet_stop_event.csv",
+  col_types = cols(
+    pattern_distance = col_double(),
+    service_date = col_datetime(format = "%d%b%Y:%H:%M:%S")
+  )
+) %>%
+  filter(!is.na(service_key)) %>%
+  select(
+    -vehicle_number,
+    -badge,
+    -trip_number
+  )
+gc()
+
+trimet_stop_event %<>%
+  mutate(
+    stop_hours = stop_time / 3600.0,
+    arrive_hours = arrive_time / 3600.0,
+    leave_hours = leave_time / 3600.0
+  )
+gc()
+
+trimet_stop_event %<>%
+  arrange(
+    route_number,
+    service_date,
+    arrive_time
+  )
+gc()
+
+trimet_stop_event %>%
+  write_csv(path = "../../data/interim/tidy_trimet_stop_event.csv")
