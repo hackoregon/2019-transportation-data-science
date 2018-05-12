65478cafdca105bb73d919022ae3f52314af9c82
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-12 16:35:36 -0700

    get rid of the hour of day calculations - computable when needed

--- src/data/load_congestion_data.R
+++ src/data/load_congestion_data.R
@@ -1,118 +1,8 @@
-# function definitions
-
-## load one of the trimet_stop_event CSV files
-#' load_csv
-#'
-#' @param path path to a TriMet "trimet_stop_event" CSV file
-#'
-#' @return a tibble with the data from the file, with some added columns
-
-load_csv <- function(path) {
-  read_csv(
-    path,
-    col_types = cols(
-      SERVICE_DATE = col_date(format = "%d%b%Y:%H:%M:%S"),
-      PATTERN_DISTANCE = col_double()
-    )
-  )
-}
-
-## drop unusued columns
-drop_unused_columns <- function(stop_events) {
-
-  stop_events %>% select(
-    -BADGE,
-    -MAXIMUM_SPEED
-    -TRAIN_MILEAGE
-    -PATTERN_DISTANCE
-    -LOCATION_DISTANCE
-    -X_COORDINATE
-    -Y_COORDINATE
-    -DATA_SOURCE
-    -SCHEDULE_STATUS
-  )
-}
-
-## filter unwanted rows
-filter_unwanted_rows <- function(stop_events) {
-  stop_events %>% filter(
-    LOCATION_ID > 0,
-    ROUTE_NUMBER > 0,
-    ROUTE_NUMBER <= 291,
-    SERVICE_KEY == "W" |
-      SERVICE_KEY == "S" |
-      SERVICE_KEY == "U" |
-      SERVICE_KEY == "X"
-  )
-}
-
-## add conventiences for analysis
-add_conveniences <- function(stop_events) {
-
-  temp <- stop_events
-
-  # convert the service data to a character string
-  temp$SERVICE_DATE <- as.character(temp$SERVICE_DATE)
-
-  temp <- temp %>% mutate(
-    WEEKDAY = lubridate::wday(SERVICE_DATE),
-    STOP_HOURS = round(STOP_TIME / 3600.0),
-    ARRIVE_HOURS = round(ARRIVE_TIME / 3600.0),
-    LEAVE_HOURS = round(LEAVE_TIME / 3600.0),
-    TRIP_KEY = paste(
-      SERVICE_DATE,
-      VEHICLE_NUMBER,
-      ROUTE_NUMBER,
-      DIRECTION,
-      TRIP_NUMBER,
-      sep = "_"
-    )
-  )
-  return(temp)
-}
-
-#' Group by trips
-#'
-#' @param stop_events a "stop events" tibble
-#'
-#' @return the tibble grouped by trips
-group_by_trips <- function(stop_events) {
-
-  # sort first to get each trip in chronological order
-  stop_events %>% arrange(
-    VEHICLE_NUMBER,
-    SERVICE_DATE,
-    ARRIVE_TIME
-  ) %>%
-    group_by(TRIP_KEY)
-}
-
-#' Compute lagged columns
-#' Omce we have the data grouped by trips, we want to add a column for the previous location ID and the time from when the vehicle left there to when it arrived here
-#'
-#' @param stop_events a stop_events tibble
-#'
-#' @return the tibble with the new columns
-#'
-compute_lagged_columns <- function(stop_events) {
-  stop_events %>%
-    mutate(
-      SECONDS_LATE = ARRIVE_TIME - STOP_TIME,
-      FROM_LOCATION = lag(LOCATION_ID),
-      LEFT_THERE = lag(LEAVE_TIME),
-      TRAVEL_SECONDS = ARRIVE_TIME - LEFT_THERE
-   ) %>%
-   filter(
-     !is.na(TRAVEL_SECONDS),
-     TRAVEL_SECONDS > 0
-   )
-}
-
-# Main program
-
 ## load libraries
 if (!require(tidyverse)) install.packages("tidyverse")
 library(tidyverse)
+setwd("/d/Projects/transportation-congestion-analysis/src/data")
+source("function_definitions.R")
 
 ## define the month table
 month_table <- tribble(
@@ -123,34 +13,35 @@ month_table <- tribble(
 )
 
 ## loop over months
-for (i in 1:nrow(month_table)) {
+#for (i in 1:nrow(month_table)) {
+for (i in 1) {
 
   gc(full = TRUE, verbose = TRUE)
   trimet_stop_events <- load_csv(
     paste("../../data/raw", month_table$input_file[i], sep = "/")
   )
-
   gc(full = TRUE, verbose = TRUE)
+
   trimet_stop_events <- trimet_stop_events %>%
     drop_unused_columns()
-
   gc(full = TRUE, verbose = TRUE)
+
   trimet_stop_events <- trimet_stop_events %>%
     filter_unwanted_rows()
-
   gc(full = TRUE, verbose = TRUE)
+
   trimet_stop_events <- trimet_stop_events %>%
     add_conveniences()
-
   gc(full = TRUE, verbose = TRUE)
+
   trimet_stop_events <- trimet_stop_events %>%
     group_by_trips()
-
   gc(full = TRUE, verbose = TRUE)
+
   trimet_stop_events <- trimet_stop_events %>%
     compute_lagged_columns()
-
   gc(full = TRUE, verbose = TRUE)
+
   edge_data <- trimet_stop_events %>% select(
     SERVICE_DATE,
     ROUTE_NUMBER,
