b3bc564796425cba17a29de88f78786a13b93567
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-14 21:26:13 -0700

    clean up paths

--- src/data/run-from-linux.bash
+++ src/data/run-from-linux.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# raw data
+# make sure raw data exists
 if [ ! -e ../../data/raw/scrapes.rar ]
 then
   echo "Input file ../../data/raw/scrapes.rar does not exist - exiting!"
@@ -13,9 +13,10 @@ then
 fi
 
 echo "Copying raw data to the container"
-docker cp ../../data/raw containers_postgis_1:/home/dbsuper/Raw
+docker cp ../../data/raw/scrapes.rar containers_postgis_1:/home/dbsuper/Raw
+docker cp ../../data/raw/passenger_census.csv containers_postgis_1:/home/dbsuper/Raw
 
-echo "Copying the scripts to the container"
+echo "Copying processing scripts to the container"
 docker cp trimet_database_scripts containers_postgis_1:/home/dbsuper
 
 exit
@@ -24,6 +25,5 @@ echo ""
 echo ""
 docker exec -it -u dbsuper -w /home/dbsuper/trimet_database_scripts containers_postgis_1 ./create_database.bash
 
-echo "Retriving the backups"
-docker cp containers_postgis_1:home/dbsuper/Raw/transportation-systems-trimet.sql.gz ../../data/interim/
-ls -ltr ../../containers/Raw
+echo "Retriving the backup"
+docker cp containers_postgis_1:home/dbsuper/Raw/transportation-systems-trimet.sql.gz ../../data/interim
