4784190576109e736820b3b9b3d976213e70721e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-14 20:56:06 -0700

    manual merge in process

--- src/data/trimet_database_scripts/2create-database.bash
+++ src/data/trimet_database_scripts/2create-database.bash
@@ -1,28 +1,19 @@
 #! /bin/bash
 
-# get connection details
-source .env
-
 # define parameters
-export interim="../../data/interim"
-export external="../../data/external"
-export DBOWNER=postgres
+export DBOWNER=transportation-systems
 export PGDATABASE=transportation-systems-trimet
 
 # create a fresh database
 echo "Creating ${PGDATABASE} - ignore error if it doesn't exist"
 dropdb ${PGDATABASE} || true
-sudo mkdir -p /ssdpg
-sudo chown -R postgres:postgres /ssdpg
-psql -c "CREATE TABLESPACE ssdpg LOCATION '/ssdpg';"
-createdb --owner=${DBOWNER} --tablespace=ssdpg ${PGDATABASE}
+createdb --owner=${DBOWNER} ${PGDATABASE}
 psql -d ${PGDATABASE} -c "CREATE EXTENSION postgis CASCADE;"
 
 # import TriMet shapefiles
 echo "Importing TriMet shapefiles"
-pushd ${external}
+pushd ~/Raw
 export WHERE=https://developer.trimet.org/gis/data
-psql -d ${PGDATABASE} -c "CREATE SCHEMA trimet_gis;"
 for table in tm_boundary tm_parkride tm_rail_lines tm_rail_stops tm_routes tm_stops tm_route_stops tm_tran_cen
 do
   echo "Importing $table"
@@ -30,12 +21,21 @@ do
   rm -fr $table; mkdir $table; cd $table
   unzip -qq ../$table.zip
   ogr2ogr -f PostgreSQL -lco PRECISION=NO -nlt PROMOTE_TO_MULTI \
-    -lco SCHEMA=trimet_gis PG:"dbname=${PGDATABASE}" $table.shp
+    PG:"dbname=${PGDATABASE}" $table.shp
   cd ..
 done
 popd
 
+echo "Creating the 'passenger_census' (ridership data) table"
+psql -f passenger-census.psql
+
 echo "Creating the 'trimet_stop_events' table"
+
+pushd ~/Raw
+echo "Unpacking the archive"
+/usr/bin/time 7z e -y scrapes.rar
+popd
+
 psql -d ${PGDATABASE} -f "trimet_stop_events.ddl"
 echo "Loading the data"
 export copy_command="\copy trimet_stop_events from '${interim}/trimet_stop_events.csv' with csv header"
