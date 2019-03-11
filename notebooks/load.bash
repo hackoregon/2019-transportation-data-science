ac6d413ae81b764f39c05836fb6e5877865e17a1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-11 11:57:42 -0700

    time the \copy step

--- notebooks/load.bash
+++ notebooks/load.bash
@@ -2,4 +2,4 @@
 
 psql -d postgres -f create_database.sql
 psql -d transit_operations_analytics_data -f create_tables.sql
-psql -d transit_operations_analytics_data -f copy_tables.sql
+/usr/bin/time psql -d transit_operations_analytics_data -f copy_tables.sql
