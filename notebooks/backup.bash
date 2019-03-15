9520aa447182d7440350811999eace4a8fe0f5ea
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-14 23:33:20 -0700

    compress

--- notebooks/backup.bash
+++ notebooks/backup.bash
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 /usr/bin/time pg_dump \
-  --file=transit_operations_analytics_data.sql \
+  --port=5440 \
   --format=p \
   --verbose \
-  --dbname=transit_operations_analytics_data
+  --dbname=transit_operations_analytics_data \
+  | gzip -c > transit_operations_analytics_data.sql.gz
