e5ca232334cc4a88d11cf0324322705cff47e8e6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-14 21:05:06 -0700

    add CSV stacks of all months

--- notebooks/extract.bash
+++ notebooks/extract.bash
@@ -11,4 +11,35 @@ rm *.csv *.xlsx *.txt
 unrar x scrapes.rar "*.csv"
 unrar x April\ 2018.rar "*.csv"
 unrar x May\ 2018.rar
+
+# concatenate
+/usr/bin/time grep -v '_DATE,' \
+  "init_cyclic_v1h 1-30SEP2017.csv" \
+  "init_cyclic_v1h 1-31OCT2017.csv" \
+  "init_cyclic_v1h 1-30NOV2017.csv" \
+  "init_cyclic_v1h 1-30APR2018.csv" \
+  "init_cyclic_v1h 1-31MAY2018.csv" \
+  > init_cyclic_v1h.csv
+/usr/bin/time grep -v '_DATE,' \
+  "init_tripsh 1-30SEP2017.csv" \
+  "init_tripsh 1-31OCT2017.csv" \
+  "init_tripsh 1-30NOV2017.csv" \
+  "init_tripsh 1-30APR2018.csv" \
+  "init_tripsh 1-31MAY2018.csv" \
+  > init_tripsh.csv
+/usr/bin/time grep -v '_DATE,' \
+  "init_veh_stoph 1-30SEP2017.csv" \
+  "init_veh_stoph 1-31OCT2017.csv" \
+  "init_veh_stoph 1-30NOV2017.csv" \
+  "init_veh_stoph 1-30APR2018.csv" \
+  "init_veh_stoph 1-31MAY2018.csv" \
+  > init_veh_stoph.csv
+/usr/bin/time grep -v '_DATE,' \
+  "trimet_stop_event 1-30SEP2017.csv" \
+  "trimet_stop_event 1-31OCT2017.csv" \
+  "trimet_stop_event 1-30NOV2017.csv" \
+  "trimet_stop_event 1-30APR2018.csv" \
+  "trimet_stop_event 1-31MAY2018.csv" \
+  > trimet_stop_event.csv
+
 popd
