41cb944eb49a83440ac9879cd64221fb50d173aa
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-15 06:43:50 -0700

    time everything

--- notebooks/extract.bash
+++ notebooks/extract.bash
@@ -2,15 +2,15 @@
 
 # checksum input archives
 pushd "/home/znmeb/Raw/transportation-2018/transit-operations-analytics-data"
-sha512sum -c scrapes.rar.sha512sum
-sha512sum -c April\ 2018.rar.sha512sum
-sha512sum -c May\ 2018.rar.sha512sum
+/usr/bin/time sha512sum -c scrapes.rar.sha512sum
+/usr/bin/time sha512sum -c April\ 2018.rar.sha512sum
+/usr/bin/time sha512sum -c May\ 2018.rar.sha512sum
 
 # extract raw CSVs
 rm *.csv *.xlsx *.txt
-unrar x scrapes.rar "*.csv"
-unrar x April\ 2018.rar "*.csv"
-unrar x May\ 2018.rar
+/usr/bin/time unrar x scrapes.rar "*.csv"
+/usr/bin/time unrar x April\ 2018.rar "*.csv"
+/usr/bin/time unrar x May\ 2018.rar
 for file in *.csv *.txt
 do
   /usr/bin/time dos2unix ${file}
