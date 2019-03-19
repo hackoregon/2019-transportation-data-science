cbf3fbaa8c2e3042cc729eada838aa099973c9d6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-19 03:45:32 -0700

    get correct table names

--- src/data/fix_dates.sql
+++ src/data/fix_dates.sql
@@ -1,5 +1,5 @@
 ALTER TABLE init_veh_stoph ADD COLUMN date_stamp date;
-UPDATE init_cyclic_v1h SET date_stamp = to_date(opd_date, 'DDMONYY');
+UPDATE init_veh_stoph SET date_stamp = to_date(opd_date, 'DDMONYY');
 
 ALTER TABLE trimet_stop_event ADD COLUMN date_stamp date;
-UPDATE init_cyclic_v1h SET date_stamp = to_date(service_date, 'DDMONYY');
+UPDATE trimet_stop_event SET date_stamp = to_date(service_date, 'DDMONYY');
