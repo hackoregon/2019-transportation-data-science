206b932637980c5577242d8c8545b5c3c8e05ac8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-19 02:55:09 -0700

    use /tmp for portability

--- src/data/2import-trimet-shapefiles.bash
+++ src/data/2import-trimet-shapefiles.bash
@@ -5,7 +5,7 @@ export DBOWNER=postgres
 export PGDATABASE=transit_operations_analytics_data
 
 echo "Importing ***current*** TriMet shapefiles"
-pushd ${TMPDIR}
+pushd /tmp
 export WHERE=https://developer.trimet.org/gis/data
 for table in tm_boundary tm_parkride tm_rail_lines tm_rail_stops tm_routes tm_stops tm_route_stops tm_tran_cen
 do
