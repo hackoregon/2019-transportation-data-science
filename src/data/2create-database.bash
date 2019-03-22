426ff43417f4b82c76eea8ab8ad295848caef869
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-21 23:10:31 -0700

    do all five months

--- src/data/2create-database.bash
+++ src/data/2create-database.bash
@@ -11,14 +11,22 @@ createdb --owner=${DBOWNER} ${PGDATABASE}
 echo "Creating 'postgis' extension"
 psql -U ${DBOWNER} -d ${PGDATABASE} -c "CREATE EXTENSION postgis CASCADE;"
 
-psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
+/usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
  -v init_veh_stoph=y2017_m09_init_veh_stoph -v init_veh_stoph_csv="'/csvs/init_veh_stoph 1-30SEP2017.csv'" \
  -v trimet_stop_event=y2017_m09_trimet_stop_event -v trimet_stop_event_csv="'/csvs/trimet_stop_event 1-30SEP2017.csv'"
 
-psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
+/usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
  -v init_veh_stoph=y2017_m10_init_veh_stoph -v init_veh_stoph_csv="'/csvs/init_veh_stoph 1-31OCT2017.csv'" \
  -v trimet_stop_event=y2017_m10_trimet_stop_event -v trimet_stop_event_csv="'/csvs/trimet_stop_event 1-31OCT2017.csv'"
 
-psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
+/usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
  -v init_veh_stoph=y2017_m11_init_veh_stoph -v init_veh_stoph_csv="'/csvs/init_veh_stoph 1-30NOV2017.csv'" \
  -v trimet_stop_event=y2017_m11_trimet_stop_event -v trimet_stop_event_csv="'/csvs/trimet_stop_event 1-30NOV2017.csv'"
+
+/usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
+ -v init_veh_stoph=y2018_m04_init_veh_stoph -v init_veh_stoph_csv="'/csvs/init_veh_stoph 1-30APR2018.csv'" \
+ -v trimet_stop_event=y2018_m04_trimet_stop_event -v trimet_stop_event_csv="'/csvs/trimet_stop_event 1-30APR2018.csv'"
+
+/usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
+ -v init_veh_stoph=y2018_m05_init_veh_stoph -v init_veh_stoph_csv="'/csvs/init_veh_stoph 1-31MAY2018.csv'" \
+ -v trimet_stop_event=y2018_m05_trimet_stop_event -v trimet_stop_event_csv="'/csvs/trimet_stop_event 1-31MAY2018.csv'"
