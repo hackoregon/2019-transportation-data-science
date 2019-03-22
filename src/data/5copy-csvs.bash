a6e133dee339bc8388e6dc402a60d2e68797c696
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-21 23:09:10 -0700

    don't need shapefiles

--- src/data/5copy-csvs.bash
+++ src/data/5copy-csvs.bash
@@ -4,10 +4,6 @@
 export DBOWNER=postgres
 export PGDATABASE=transit_operations_analytics_data
 
-echo "Creating the tables"
-/usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql
-echo "Copying the CSVs to PostGIS"
-/usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f copy_tables.sql
 echo "Creating date stamps"
 /usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f fix_dates.sql
 echo "Creating geometry columns"
