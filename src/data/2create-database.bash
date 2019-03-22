b8ec8071d1fbc3bd8be25008d25539e0b17b63fb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-21 22:23:53 -0700

    copy after create

--- src/data/2create-database.bash
+++ src/data/2create-database.bash
@@ -10,3 +10,15 @@ dropdb ${PGDATABASE} || true
 createdb --owner=${DBOWNER} ${PGDATABASE}
 echo "Creating 'postgis' extension"
 psql -U ${DBOWNER} -d ${PGDATABASE} -c "CREATE EXTENSION postgis CASCADE;"
+
+psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
+ -v init_veh_stoph=y2017_m09_init_veh_stoph -v init_veh_stoph_csv="'/csvs/init_veh_stoph 1-30SEP2017.csv'" \
+ -v trimet_stop_event=y2017_m09_trimet_stop_event -v trimet_stop_event_csv="'/csvs/trimet_stop_event 1-30SEP2017.csv'"
+
+psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
+ -v init_veh_stoph=y2017_m10_init_veh_stoph -v init_veh_stoph_csv="'/csvs/init_veh_stoph 1-31OCT2017.csv'" \
+ -v trimet_stop_event=y2017_m10_trimet_stop_event -v trimet_stop_event_csv="'/csvs/trimet_stop_event 1-31OCT2017.csv'"
+
+psql -U ${DBOWNER} -d ${PGDATABASE} -f create_tables.sql \
+ -v init_veh_stoph=y2017_m11_init_veh_stoph -v init_veh_stoph_csv="'/csvs/init_veh_stoph 1-30NOV2017.csv'" \
+ -v trimet_stop_event=y2017_m11_trimet_stop_event -v trimet_stop_event_csv="'/csvs/trimet_stop_event 1-30NOV2017.csv'"
