2147854a9d282ac6ddba89840e86009632b35333
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-24 22:50:18 -0700

    all five months

--- src/data/4compute_runs_table.bash
+++ src/data/4compute_runs_table.bash
@@ -6,7 +6,7 @@ export PGDATABASE=transit_operations_analytics_data
 export PGPORT=5440
 
 # loop over month codes
-for schema in y2017_m09 y2017_m10 y2017_m11
+for schema in y2017_m09 y2017_m10 y2017_m11 y2018_m04 y2018_m05
 do
   /usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f compute_runs_table.sql \
  -v schema=${schema}
--- src/data/9create-database-backup.bash
+++ src/data/9create-database-backup.bash
@@ -11,4 +11,5 @@ sudo du -sh /var/lib/postgres/data/
 /usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -c "VACUUM ANALYZE;"
 sudo du -sh /var/lib/postgres/data/
 echo "Creating the database backup"
-/usr/bin/time pg_dump --format=c --dbname=${PGDATABASE} > ${DEST}/${PGDATABASE}.backup
+/usr/bin/time pg_dump --format=p --clean --if-exists --create --dbname=${PGDATABASE} | gzip -dc \
+  > ${DEST}/${PGDATABASE}.sql.gz
