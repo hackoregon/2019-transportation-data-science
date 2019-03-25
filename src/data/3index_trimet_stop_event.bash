d4a7ed0e78398072977862dd193939f403361396
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-24 17:34:45 -0700

    converted to schema by month

--- src/data/3index_trimet_stop_event.bash
+++ src/data/3index_trimet_stop_event.bash
@@ -6,8 +6,8 @@ export PGDATABASE=transit_operations_analytics_data
 export PGPORT=5440
 
 # loop over month codes
-for month in y2017_m09 y2017_m10 y2017_m11
+for schema in y2017_m09 y2017_m10 y2017_m11
 do
   /usr/bin/time psql -U ${DBOWNER} -d ${PGDATABASE} -f index_trimet_stop_event.sql \
- -v trimet_stop_event=${month}_trimet_stop_event
+ -v schema=${schema}
 done
--- src/data/index_trimet_stop_event.sql
+++ src/data/index_trimet_stop_event.sql
@@ -1,6 +1,7 @@
 \echo deleting unwanted rows
-SET search_path TO trimet_stop_event, public;
-DELETE FROM :trimet_stop_event
+\echo weekday service key is W for buses and A for MAX!
+SET search_path TO :schema, public;
+DELETE FROM trimet_stop_event
   WHERE service_key IS NULL 
   OR NOT (service_key = 'W' OR service_key = 'A')
   OR route_number IS NULL
@@ -8,33 +9,11 @@ DELETE FROM :trimet_stop_event
   OR route_number < 1
 ;
 
-\echo dropping unwanted columns
-ALTER TABLE :trimet_stop_event DROP COLUMN IF EXISTS badge;
-ALTER TABLE :trimet_stop_event DROP COLUMN IF EXISTS maximum_speed;
-ALTER TABLE :trimet_stop_event DROP COLUMN IF EXISTS pattern_distance;
-ALTER TABLE :trimet_stop_event DROP COLUMN IF EXISTS location_distance;
-ALTER TABLE :trimet_stop_event DROP COLUMN IF EXISTS data_source;
-ALTER TABLE :trimet_stop_event DROP COLUMN IF EXISTS schedule_status;
-
-\echo making a usable date stamp
-ALTER TABLE :trimet_stop_event ADD COLUMN date_stamp date;
-UPDATE :trimet_stop_event SET date_stamp = to_date(service_date, 'DDMONYY');
-ALTER TABLE :trimet_stop_event DROP COLUMN IF EXISTS service_date;
-
 \echo indexing the variables that define events
-CREATE INDEX ON :trimet_stop_event (date_stamp);
-CREATE INDEX ON :trimet_stop_event (vehicle_number);
-CREATE INDEX ON :trimet_stop_event (leave_time);
-CREATE INDEX ON :trimet_stop_event (route_number);
-CREATE INDEX ON :trimet_stop_event (direction);
-CREATE INDEX ON :trimet_stop_event (arrive_time);
-CREATE INDEX ON :trimet_stop_event (location_id);
-
-\echo adding primary key
-ALTER TABLE :trimet_stop_event ADD PRIMARY KEY 
-  (date_stamp, vehicle_number, route_number, direction, arrive_time, leave_time);
--- \echo adding indexed geometry column
--- ALTER TABLE :trimet_stop_event ADD COLUMN geom_point_4326 geometry;
--- UPDATE :trimet_stop_event
-  -- SET geom_point_4326 = ST_Transform(ST_SetSRID(ST_Point(x_coordinate, y_coordinate), 2913), 4326);
--- CREATE INDEX ON :trimet_stop_event USING GIST (geom_point_4326);
+CREATE INDEX ON trimet_stop_event (service_date);
+CREATE INDEX ON trimet_stop_event (vehicle_number);
+CREATE INDEX ON trimet_stop_event (leave_time);
+CREATE INDEX ON trimet_stop_event (route_number);
+CREATE INDEX ON trimet_stop_event (direction);
+CREATE INDEX ON trimet_stop_event (arrive_time);
+CREATE INDEX ON trimet_stop_event (location_id);
