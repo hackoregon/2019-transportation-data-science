97edeceeff5994b2c92d810df129cd961bdc2b3e
Jeff Beyer <jeffrey.beyer@nike.com>
2019-03-24 19:07:08 -0700

    Adds sql queries for hawthorne analysis by month

---
+++ references/may_2018_export.sql
@@ -0,0 +1,12 @@
+select * from "trimet_stop_event 1-31MAY2018" 
+	inner join "init_veh_stoph 1-31MAY2018" on 
+		("trimet_stop_event 1-31MAY2018".SERVICE_DATE = "init_veh_stoph 1-31MAY2018".OPD_Date) and 
+		("init_veh_stoph 1-31MAY2018".POINT_ID = "trimet_stop_event 1-31MAY2018".LOCATION_ID) and
+		("init_veh_stoph 1-31MAY2018".NOM_ARR_TIME = "trimet_stop_event 1-31MAY2018".STOP_TIME) and
+		("init_veh_stoph 1-31MAY2018".ACT_ARR_TIME = "trimet_stop_event 1-31MAY2018".ARRIVE_TIME) and
+		(1*"init_veh_stoph 1-31MAY2018".VEHICLE_ID = 1*"trimet_stop_event 1-31MAY2018".VEHICLE_NUMBER)
+	where "trimet_stop_event 1-31MAY2018".ROUTE_NUMBER in ("4", "10", "14") AND
+		"init_veh_stoph 1-31MAY2018".NOM_ARR_TIME is not '' AND 
+		"trimet_stop_event 1-31MAY2018".LOCATION_ID in ("3637", "3641", "3633", "2642", "7856") AND
+		"trimet_stop_event 1-31MAY2018".SERVICE_KEY is "W" AND
+		1*"trimet_stop_event 1-31MAY2018".ARRIVE_TIME between 18000 AND 43200
---
+++ references/nov_2017_export.sql
@@ -0,0 +1,14 @@
+select * from "trimet_stop_event 1-30NOV2017" 
+	inner join "init_veh_stoph 1-30NOV2017" on 
+		("trimet_stop_event 1-30NOV2017".SERVICE_DATE = "init_veh_stoph 1-30NOV2017".OPD_Date) and 
+		("init_veh_stoph 1-30NOV2017".POINT_ID = "trimet_stop_event 1-30NOV2017".LOCATION_ID) and
+		("init_veh_stoph 1-30NOV2017".NOM_ARR_TIME = "trimet_stop_event 1-30NOV2017".STOP_TIME) and
+		("init_veh_stoph 1-30NOV2017".ACT_ARR_TIME = "trimet_stop_event 1-30NOV2017".ARRIVE_TIME) and
+		(1*"init_veh_stoph 1-30NOV2017".VEHICLE_ID = 1*"trimet_stop_event 1-30NOV2017".VEHICLE_NUMBER)
+	where "trimet_stop_event 1-30NOV2017".ROUTE_NUMBER in ("4", "10", "14") AND
+		"init_veh_stoph 1-30NOV2017".NOM_ARR_TIME is not '' AND 
+		"trimet_stop_event 1-30NOV2017".LOCATION_ID in ("3637", "3641", "3633", "2642", "7856") AND
+		"trimet_stop_event 1-30NOV2017".SERVICE_KEY is "W" AND
+		1*"trimet_stop_event 1-30NOV2017".ARRIVE_TIME between 18000 AND 43200
+
+
