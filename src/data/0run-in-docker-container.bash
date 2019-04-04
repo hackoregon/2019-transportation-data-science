ee22377d1e040e4def9d0f9b6f8999a00a7d0a47
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-04-04 11:46:22 -0700

    meaningful image and container names

--- src/data/0run-in-docker-container.bash
+++ src/data/0run-in-docker-container.bash
@@ -1,32 +1,33 @@
 #! /bin/bash
 
 echo "Building the image"
-docker build --tag postgis:latest .
+docker build --tag postgis-image:latest .
 docker images
 echo "Force-removing all existing containers"
 docker rm -f `docker ps -aq`
 echo "Running the container"
-docker run --detach --rm --name=postgisc --volume /csvs:/csvs \
+docker run --detach --rm --name=postgis-container --volume /csvs:/csvs \
   -c 'shared_buffers=1024MB' \
   -c 'work_mem=256MB' \
   -c 'maintenance_work_mem=256MB' \
   -c 'checkpoint_timeout=30min' \
   -c 'max_wal_size=30GB' \
-  postgis
+  postgis-image
 docker ps
 echo "Copying the scripts"
-docker cp . postgisc:/src
-docker exec --user=root postgisc chown -R postgres:postgres /src
+docker cp . postgis-container:/src
+docker exec --user=root postgis-container chown -R postgres:postgres /src
 echo "Chowning the input CSVs"
-docker exec --user=root postgisc chown -R postgres:postgres /csvs
+docker exec --user=root postgis-container chown -R postgres:postgres /csvs
 echo "Loading the database"
 sleep 30
-docker exec --user=postgres --workdir=/src postgisc /src/2load.bash
-docker exec --user=postgres --workdir=/src postgisc /src/9create-database-backup.bash
+docker exec --user=postgres --workdir=/src postgis-container /src/2load.bash
+docker exec --user=postgres --workdir=/src postgis-container /src/9create-database-backup.bash
 echo "Retrieving the backup"
-docker cp postgisc:/csvs/transit_operations_analytics_data.sql.gz .
-docker cp postgisc:/csvs/transit_operations_analytics_data.sql.gz.sha512sum .
+docker cp postgis-container:/csvs/transit_operations_analytics_data.sql.gz .
+docker cp postgis-container:/csvs/transit_operations_analytics_data.sql.gz.sha512sum .
 sha512sum -c transit_operations_analytics_data.sql.gz.sha512sum
 echo "Retrieving database schema"
-docker exec --user=postgres --workdir=/src/ postgisc postgresql_autodoc -d transit_operations_analytics_data -t html
-docker cp postgisc:/src/transit_operations_analytics_data.html .
+docker exec --user=postgres --workdir=/src/ postgis-container \
+  postgresql_autodoc -d transit_operations_analytics_data -t html
+docker cp postgis-container:/src/transit_operations_analytics_data.html .
