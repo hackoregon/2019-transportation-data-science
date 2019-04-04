2f0fa7b5baf2f66ba1dd21660387f4e2a8920d40
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-04-04 11:35:31 -0700

    postpone creating extension till needed

--- src/data/2load.bash
+++ src/data/2load.bash
@@ -8,8 +8,6 @@ export PGDATABASE=transit_operations_analytics_data
 echo "Creating fresh database ${PGDATABASE} - ignore error if it doesn't exist"
 dropdb ${PGDATABASE} || true
 createdb --owner=${DBOWNER} ${PGDATABASE}
-echo "Creating 'postgis' extension"
-psql -U ${DBOWNER} -d ${PGDATABASE} -c "CREATE EXTENSION postgis CASCADE;"
 for sqlfile in create_*.sql
 do
   psql -U ${DBOWNER} -d ${PGDATABASE} -f ${sqlfile}
