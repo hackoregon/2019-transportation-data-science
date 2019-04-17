961275690e46ffb25a125d5ffeeff2765b3344ce
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-04-17 00:39:27 -0700

    connect to local database

--- src/toad_api/toad_api/settings.py
+++ src/toad_api/toad_api/settings.py
@@ -73,10 +73,19 @@ WSGI_APPLICATION = 'toad_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transit_operations_analytics_data',
+        'USER': 'postgres',
+        'HOST': '/var/run/postgresql',
+        'PORT': 5432,
     }
 }
 
