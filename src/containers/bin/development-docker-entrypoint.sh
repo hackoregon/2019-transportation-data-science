7a401b9d3f15cd5a23dfdc38cf2c791689853ad1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-04-30 22:55:04 -0700

    30 seconds between checks for PostgreSQL

--- src/containers/bin/development-docker-entrypoint.sh
+++ src/containers/bin/development-docker-entrypoint.sh
@@ -11,7 +11,7 @@ export PGPASSWORD=$POSTGRES_PASSWORD
 until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d postgres -c '\q'
 do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 5
+  sleep 30
 done
 
 >&2 echo "Postgres is up"
