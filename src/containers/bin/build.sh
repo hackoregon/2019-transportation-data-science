770297b97ca765eb9a8419dabbfffd41392d89f2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-04-30 22:52:42 -0700

    ditch production - that comes later

--- src/containers/bin/build.sh
+++ src/containers/bin/build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-usage() { echo "Usage: $0 [-d] for a development build, [-p] for a production build" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-d] for a development build" 1>&2; exit 1; }
 
 if [ $# == 0 ]; then usage; fi
 
@@ -7,9 +7,6 @@ while getopts ":dp" opt; do
     case "$opt" in
         d)
           docker-compose -f development-docker-compose.yml build
-          ;;
-        p)
-          docker-compose -f production-docker-compose.yml build
           ;;
         *)
           usage
