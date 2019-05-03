e259e2c6b39c51956c686cacbdf31c921ed0db33
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-02 19:46:58 -0700

    geotagging code

--- src/data/0runall.bash
+++ src/data/0runall.bash
@@ -3,5 +3,6 @@
 ./1extract.bash
 ./2load.bash
 ./3build_model.bash
-./4cleanup.bash
+./4geotag.bash
+./5cleanup.bash
 ./9create-database-backup.bash
---
+++ src/data/4geotag.bash
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# define parameters
+export DBOWNER=transportation2019
+export PGDATABASE=transit_operations_analytics_data
+
+psql -U ${DBOWNER} -d ${PGDATABASE} -f geotag.sql
