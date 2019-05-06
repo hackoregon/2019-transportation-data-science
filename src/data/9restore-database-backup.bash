9a8a703e34a9b20ff9aa60ea2f29cd33eeb249c5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-05 23:03:28 -0700

    restore script to test ordinary user restores

---
+++ src/data/9restore-database-backup.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+export BACKUP_PATH=/data/container-csvs
+pushd ${BACKUP_PATH}
+
+echo "Creating user and database with PostGIS as database superuser"
+sudo su - postgres -c "dropdb transit_operations_analytics_data"
+sudo su - postgres -c "dropuser transportation2019"
+sudo su - postgres -c "createuser transportation2019"
+sudo su - postgres -c "createdb --owner=transportation2019 transit_operations_analytics_data"
+sudo su - postgres -c "psql -d transit_operations_analytics_data -c 'CREATE EXTENSION postgis;'"
+
+echo "Checksumming backup file"
+sha512sum -c transit_operations_analytics_data.backup.sha512sum
+echo "Restoring database as ordinary user - ignore error when it tries to create extension"
+pg_restore --verbose --dbname=transit_operations_analytics_data --username=transportation2019 \
+  transit_operations_analytics_data.backup
+sudo su - postgres -c "psql -d transit_operations_analytics_data -c '\dS+'"
+
+popd
