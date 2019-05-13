b69130792b1eccae550f9a1acce8baa714f4b1d1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-13 14:03:38 -0700

    admin is working!

--- src/toad_api/toad_api_app/admin.py
+++ src/toad_api/toad_api_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import BusAllStops
+from .models import BusPassengerStops
+from .models import BusTrips
+from .models import RailPassengerStops
+
+admin.site.register(BusAllStops)
+admin.site.register(BusPassengerStops)
+admin.site.register(BusTrips)
+admin.site.register(RailPassengerStops)
