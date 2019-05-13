5fc01edd1adda7d6bee54f241cd756d9d71b7243
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-13 14:25:21 -0700

    views coded

--- src/toad_api/toad_api_app/views.py
+++ src/toad_api/toad_api_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from .models import BusAllStops
+from .models import BusPassengerStops
+from .models import BusTrips
+from .models import RailPassengerStops
+
+class BusAllStopsListView(ListView):
+	model = BusAllStops
+        template_name = 'bus_all_stops_list.html'
+class BusPassengerStopsListView(ListView):
+	model = BusPassengerStops
+        template_name = 'bus_passenger_stops_list.html'
+class BusTripsListView(ListView):
+	model = BusTrips
+        template_name = 'bus_trips_list.html'
+class RailPassengerStopsListView(ListView):
+	model = RailPassengerStops
+        template_name = 'rail_passenger_stops_list.html'
