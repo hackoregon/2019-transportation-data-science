a0b54a8a5e7947e68f4a8444af370fa3ecaaf43b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-20 12:23:09 -0700

    switch to bicycle

--- src/open-source-routing-machine/demo-osrm-oregon.bash
+++ src/open-source-routing-machine/demo-osrm-oregon.bash
@@ -1,17 +1,20 @@
 #! /bin/bash
 
+echo "Downloading Oregon OpenStreetMap data"
 wget -nc https://download.geofabrik.de/north-america/us/oregon-latest.osm.pbf
-sudo docker run --privileged -t -v "${PWD}:/data" \
-  osrm/osrm-backend osrm-extract -p /opt/car.lua \
+echo "Processing bicycle routing network"
+sudo docker run --privileged --rm -t -v "${PWD}:/data" \
+  osrm/osrm-backend osrm-extract -p /opt/bicycle.lua \
   /data/oregon-latest.osm.pbf
-sudo docker run --privileged -t -v "${PWD}:/data" \
+sudo docker run --privileged --rm -t -v "${PWD}:/data" \
   osrm/osrm-backend osrm-partition \
   /data/oregon-latest.osrm
-sudo docker run --privileged -t -v "${PWD}:/data" \
+sudo docker run --privileged --rm -t -v "${PWD}:/data" \
   osrm/osrm-backend osrm-customize \
   /data/oregon-latest.osrm
+echo "Starting routing server"
 sudo docker run --privileged -d -p 5000:5000 -v "${PWD}:/data" \
   osrm/osrm-backend osrm-routed --algorithm mld \
   /data/oregon-latest.osrm
+echo "Starting map front end server"
 sudo docker run --privileged -d -p 9966:9966 osrm/osrm-frontend
-xdg-open 'http://127.0.0.1:9966'
