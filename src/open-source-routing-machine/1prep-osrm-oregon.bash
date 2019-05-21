8f5408b86d98ee0d4554187b3473098e6d5f7898
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-20 22:42:45 -0700

    spell out flags

--- src/open-source-routing-machine/1prep-osrm-oregon.bash
+++ src/open-source-routing-machine/1prep-osrm-oregon.bash
@@ -3,12 +3,9 @@
 echo "Downloading Oregon OpenStreetMap data"
 wget -nc https://download.geofabrik.de/north-america/us/oregon-latest.osm.pbf
 echo "Processing bicycle routing network"
-sudo docker run --privileged --rm -t -v "${PWD}:/data" \
-  osrm/osrm-backend osrm-extract -p /opt/bicycle.lua \
-  /data/oregon-latest.osm.pbf
-sudo docker run --privileged --rm -t -v "${PWD}:/data" \
-  osrm/osrm-backend osrm-partition \
-  /data/oregon-latest.osrm
-sudo docker run --privileged --rm -t -v "${PWD}:/data" \
-  osrm/osrm-backend osrm-customize \
-  /data/oregon-latest.osrm
+sudo docker run --privileged --rm --tty --volume "${PWD}:/data" \
+  osrm/osrm-backend osrm-extract -p /opt/bicycle.lua /data/oregon-latest.osm.pbf
+sudo docker run --privileged --rm --tty --volume "${PWD}:/data" \
+  osrm/osrm-backend osrm-partition /data/oregon-latest.osrm
+sudo docker run --privileged --rm --tty --volume "${PWD}:/data" \
+  osrm/osrm-backend osrm-customize /data/oregon-latest.osrm
