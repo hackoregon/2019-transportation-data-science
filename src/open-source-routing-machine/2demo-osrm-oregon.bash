a66311242aa107a00a86f91f872bc5a77bd12bf0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-06-09 15:44:16 -0700

    fix host networking on demo

--- src/open-source-routing-machine/2demo-osrm-oregon.bash
+++ src/open-source-routing-machine/2demo-osrm-oregon.bash
@@ -4,12 +4,12 @@ echo "Starting routing server"
 sudo docker rm --force osrm-backend
 sudo docker run --privileged --detach \
   --volume "${PWD}:/data" \
-  --name osrm-backend --network bridge --publish 5000:5000 \
+  --name osrm-backend --network host \
   osrm/osrm-backend osrm-routed --algorithm mld /data/oregon-latest.osrm
 echo "Starting map front end server"
 echo "Browse to http://localhost:9966 after server starts"
 sleep 15
 sudo docker rm --force osrm-frontend
 sudo docker run --tty \
-  --name osrm-frontend --network bridge --publish 9966:9966 \
+  --name osrm-frontend --network host \
   osrm/osrm-frontend
