37ee354176e64678b03df713e2fa64c91dc6582a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-06-15 20:39:03 -0700

    auxiliary data loaders

---
+++ src/data/26traffic_signals.bash
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# define parameters
+export DBOWNER=transportation2019
+export PGDATABASE=transit_operations_analytics_data
+export RAW=/Raw
+
+pushd ${RAW}
+echo "Download traffic signals shapefile from https://gis-pdx.opendata.arcgis.com/datasets/traffic-signals"
+echo "Save in ${RAW}"
+read -ep "Press 'Enter' here after file is downloaded"
+ogr2ogr -lco precision=NO -overwrite -t_srs EPSG:4326 \
+  PG:"active_schema=public" /vsizip/Traffic_Signals.zip
+popd
