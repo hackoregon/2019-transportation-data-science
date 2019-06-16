f14839b7dc8496fb65195c7d91cb17bf51652541
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-06-15 23:06:53 -0700

    just autodoc our main tables

--- src/data/90create-database-backup.bash
+++ src/data/90create-database-backup.bash
@@ -6,9 +6,10 @@ export PGDATABASE=transit_operations_analytics_data
 export DEST=/csvs
 
 echo "Creating the database backup"
-pg_dump --format=c --dbname=${PGDATABASE} > ${DEST}/${PGDATABASE}.backup
+pg_dump --verbose --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${DEST}/${PGDATABASE}.backup
 pushd ${DEST}
 sha512sum ${PGDATABASE}.backup > ${PGDATABASE}.backup.sha512sum
 echo "Creating ERD"
-postgresql_autodoc -d ${PGDATABASE} -t html
+postgresql_autodoc -d ${PGDATABASE} -t html \
+--table=bus_all_stops,bus_passenger_stops,bus_trips,disturbance_stops,rail_passenger_stops,traffic_signals
 popd
