e07716ff83ed4e6feb59ffa5f91b1e0aa66d98c9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-06-16 00:36:16 -0700

    time the backup

--- src/data/90create-database-backup.bash
+++ src/data/90create-database-backup.bash
@@ -6,8 +6,8 @@ export PGDATABASE=transit_operations_analytics_data
 export DEST=/csvs
 
 echo "Creating the database backup"
-pg_dump --verbose --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${DEST}/${PGDATABASE}.backup
 pushd ${DEST}
+/usr/bin/time pg_dump --verbose --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${PGDATABASE}.backup
 sha512sum ${PGDATABASE}.backup > ${PGDATABASE}.backup.sha512sum
 echo "Creating ERD"
 postgresql_autodoc -d ${PGDATABASE} -t html \
