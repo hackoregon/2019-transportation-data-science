3728935395ec224eb67273c53f8e70ab82d97a2e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-06-17 03:19:37 -0700

    add commented-out vacuum

--- src/data/90create_database_backup.bash
+++ src/data/90create_database_backup.bash
@@ -5,11 +5,12 @@ export PGUSER=transportation2019
 export PGDATABASE=transit_operations_analytics_data
 export DEST=/csvs
 
-echo "Vacuuming the database"
-/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --command="VACUUM ANALYZE;"
+#echo "Vacuuming the database"
+#/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --command="VACUUM ANALYZE;"
 echo "Creating the database backup"
 pushd ${DEST}
-/usr/bin/time pg_dump --verbose --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${PGDATABASE}.backup
+/usr/bin/time pg_dump --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${PGDATABASE}.backup
+pg_restore --list ${PGDATABASE}.backup
 sha512sum ${PGDATABASE}.backup > ${PGDATABASE}.backup.sha512sum
 echo "Creating ERD"
 postgresql_autodoc -d ${PGDATABASE} -t html \
