c0b248e9ee47db26d9f1950c053d1abd53482929
Jeff Beyer <jeff.beyer@gmail.com>
2019-06-23 19:34:53 -0700

    connect demo_app to dev api

--- src/demo_app/callbacks.py
+++ src/demo_app/callbacks.py
@@ -1,9 +1,4 @@
-import json
-import time
-
-import dash
-from dash.dependencies import Input, Output, State
-from dash.exceptions import PreventUpdate
+from dash.dependencies import Input, Output
 
 import functions as func
 from app import app
@@ -12,38 +7,28 @@ from app import app
 @app.callback(
     output=Output("before-map", "srcDoc"),
     inputs=[Input("time-slider", "value"), Input("month-slider", "value")],
-    state=[State("time-slider", "marks"), State("month-slider", "marks")],
 )
-def update_before_map(t_value, m_value, t_marks, m_marks):
-    """ Triggered by either slider change. Returns folium HTML string to map container. """
-    time = {
-        "start": int(t_marks[str(t_value[0])]["label"].split(" ")[0]) * 60 * 60,
-        "end": int(t_marks[str(t_value[1])]["label"].split(" ")[0]) * 60 * 60,
-    }
+def update_before_map(t_value, m_value):
+    """
+    Triggered by either slider change. Returns folium HTML string to map container.
+    """
+    _time = {"start": t_value[0], "end": t_value[1]}
 
-    month = {
-        "start": m_marks[str(m_value[0])]["label"].split(" ")[0],
-        "end": m_marks[str(m_value[1])]["label"].split(" ")[0],
-    }
+    month = {"start": m_value[0], "end": m_value[1]}
 
-    return func.update_map(map_="before", time=time, month=month)
+    return func.update_map(map_="before", time=_time, month=month, year="2017")
 
 
 @app.callback(
     output=Output("after-map", "srcDoc"),
     inputs=[Input("time-slider", "value"), Input("month-slider", "value")],
-    state=[State("time-slider", "marks"), State("month-slider", "marks")],
 )
-def update_after_map(t_value, m_value, t_marks, m_marks):
-    """ Triggered by either slider change. Returns folium HTML string to map container. """
-    time = {
-        "start": int(t_marks[str(t_value[0])]["label"].split(" ")[0]) * 60 * 60,
-        "end": int(t_marks[str(t_value[1])]["label"].split(" ")[0]) * 60 * 60,
-    }
+def update_after_map(t_value, m_value):
+    """
+    Triggered by either slider change. Returns folium HTML string to map container.
+    """
+    _time = {"start": t_value[0], "end": t_value[1]}
 
-    month = {
-        "start": m_marks[str(m_value[0])]["label"].split(" ")[0],
-        "end": m_marks[str(m_value[1])]["label"].split(" ")[0],
-    }
+    month = {"start": m_value[0], "end": m_value[1]}
 
-    return func.update_map(map_="after", time=time, month=month)
+    return func.update_map(map_="after", time=_time, month=month, year="2018")
