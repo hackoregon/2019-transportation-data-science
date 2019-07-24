997a5c5c5ac7492d3add7cd0ebd034ef5624ba29
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-07-23 21:11:17 -0700

    no tables for raw data - doing the wrangling with R/data.table/lubridate

--- src/data/container-scripts/create_partitioned_tables.sql
+++ src/data/container-scripts/create_partitioned_tables.sql
@@ -1,19 +1,19 @@
 \echo setting time zone
 SET timezone = 'PST8PDT';
 
-\echo creating tables for raw data loading
-DROP TABLE IF EXISTS raw_stop_event;
-CREATE TABLE raw_stop_event (
-  service_date text,
+\echo creating passenger_stops table
+DROP TABLE IF EXISTS passenger_stops CASCADE;
+CREATE TABLE passenger_stops (
+  service_date date not null,
   vehicle_number integer,
-  leave_time integer,
+  leave_time timestamp with time zone,
   train integer,
   route_number integer,
   direction integer,
   service_key text,
   trip_number integer,
-  stop_time integer,
-  arrive_time integer,
+  stop_time timestamp with time zone, 
+  arrive_time timestamp with time zone,
   dwell integer,
   location_id integer,
   door integer,
@@ -23,10 +23,19 @@ CREATE TABLE raw_stop_event (
   estimated_load integer,
   train_mileage double precision,
   x_coordinate double precision,
-  y_coordinate double precision
+  y_coordinate double precision,
+  year integer,
+  month integer,
+  day integer,
+  day_of_week integer,
+  id serial
+) PARTITION BY RANGE(service_date);
+CREATE INDEX ON passenger_stops (service_date);
+ALTER TABLE passenger_stops ADD PRIMARY KEY (service_date, id);
 
-DROP TABLE IF EXISTS raw_tripsh;
-CREATE TABLE raw_tripsh (
+\echo creating trips_history table
+DROP TABLE IF EXISTS trips_history CASCADE;
+CREATE TABLE trips_history (
   opd_date text,
   vehicle_id integer,
   event_no integer,
@@ -37,82 +46,37 @@ CREATE TABLE raw_tripsh (
   act_end_time integer,
   line_id integer,
   pattern_direction text
-
-DROP TABLE IF EXISTS raw_veh_stoph;
-CREATE TABLE raw_veh_stoph (
-  event_no_trip integer,
-  opd_date text,
-  vehicle_id integer,
-  meters integer,
-  act_arr_time integer,
-  act_dep_time integer,
-  nom_arr_time integer,
-  nom_dep_time integer,
-  stop_id integer,
-  stop_pos integer,
-  distance_to_next integer,
-  distance_to_trip integer,
-  doors_opening integer,
-  stop_type integer,
-  gps_longitude double precision,
-  gps_latitude double precision,
-  door_open_time integer
+) PARTITION BY RANGE(opd_date);
+CREATE INDEX ON trips_history (opd_date);
+ALTER TABLE trips_history ADD PRIMARY KEY (opd_date, id);
 
 \echo creating bus_all_stops table
 DROP TABLE IF EXISTS bus_all_stops CASCADE;
 CREATE TABLE bus_all_stops (
-  vehicle_id integer,
+  event_no_trip integer,
   opd_date date not null,
-  day_of_week integer,
-  year integer,
-  month integer,
-  day integer,
+  vehicle_id integer,
+  meters integer,
   act_arr_time timestamp with time zone, 
   act_dep_time timestamp with time zone, 
   nom_arr_time timestamp with time zone, 
   nom_dep_time timestamp with time zone, 
-  event_no_trip integer,
-  line_id integer,
-  pattern_direction character,
-  meters integer,
   stop_id integer,
   stop_pos integer,
   distance_to_next integer,
   distance_to_trip integer,
   doors_opening integer,
   stop_type integer,
-  door_open_time integer,
   gps_longitude double precision,
   gps_latitude double precision,
+  door_open_time integer,
+  line_id integer,
+  pattern_direction character,
+  year integer,
+  month integer,
+  day integer,
+  day_of_week integer,
   id serial
 ) PARTITION BY RANGE(opd_date) ;
 CREATE INDEX ON bus_all_stops (opd_date);
 ALTER TABLE bus_all_stops ADD PRIMARY KEY (opd_date, id);
-
-\echo creating passenger_stops table
-DROP TABLE IF EXISTS passenger_stops CASCADE;
-CREATE TABLE passenger_stops (
-  vehicle_id integer,
-  train integer,
-  trip_number integer,
-  service_date date not null,
-  service_key text,
-  arrive_time timestamp with time zone,
-  leave_time timestamp with time zone,
-  stop_time timestamp with time zone, 
-  route_number integer,
-  direction integer,
-  location_id integer,
-  dwell integer,
-  door integer,
-  lift integer,
-  ons integer,
-  offs integer,
-  estimated_load integer,
-  train_mileage double precision,
-  x_coordinate double precision.
-  y_coordinate double precision,
-  id serial
-) PARTITION BY RANGE(service_date);
-CREATE INDEX ON passenger_stops (service_date);
-ALTER TABLE passenger_stops ADD PRIMARY KEY (service_date, id);
