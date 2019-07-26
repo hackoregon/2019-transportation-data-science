4845a31ed4398ea1ab32290d4a67b825dd94ec5a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-07-25 23:06:47 -0700

    add some data readers / cleaners

--- src/data/container-scripts/toad_load.R
+++ src/data/container-scripts/toad_load.R
@@ -36,7 +36,41 @@ connect_toad_database <- function(
 load_csv_file <- function(work_path, file_type, month_code) {
   filename <- paste0(
     work_path, "/", file_type, "_", month_code, ".csv")
-  return(data.table::fread(filename))
+  return(data.table::fread(file = filename, showProgress = FALSE))
+}
+
+read_tripsh <- function(work_path, month_code) {
+  tripsh <- load_csv_file(work_path, file_type = "raw_tripsh", month_code)
+  invisible(gc(reset = TRUE))
+  tripsh <- tripsh[NOM_END_TIME > 0 & !is.na(ACT_END_TIME) & PATTERN_DIRECTION != ""]
+  invisible(gc(reset = TRUE))
+  tripsh <- tripsh[, `:=`(OPD_DATE = date_convert(OPD_DATE))]
+  invisible(gc(reset = TRUE))
+  tripsh <- tripsh[, `:=`(
+    NOM_DEP_TIME = NOM_DEP_TIME + OPD_DATE,
+    ACT_DEP_TIME = ACT_DEP_TIME + OPD_DATE,
+    NOM_END_TIME = NOM_END_TIME + OPD_DATE,
+    ACT_END_TIME = ACT_END_TIME + OPD_DATE
+  )]
+  invisible(gc(reset = TRUE))
+  return(tripsh)
+}
+
+read_veh_stoph <- function(work_path, month_code) {
+  veh_stoph <- load_csv_file(work_path, file_type = "raw_veh_stoph", month_code)
+  invisible(gc(reset = TRUE))
+  veh_stoph <- veh_stoph[EVENT_NO_TRIP > 0 & !is.na(NOM_ARR_TIME)]
+  invisible(gc(reset = TRUE))
+  veh_stoph <- veh_stoph[, `:=`(OPD_DATE = date_convert(OPD_DATE))]
+  invisible(gc(reset = TRUE))
+  veh_stoph <- veh_stoph[, `:=`(
+    NOM_DEP_TIME = NOM_DEP_TIME + OPD_DATE,
+    ACT_DEP_TIME = ACT_DEP_TIME + OPD_DATE,
+    NOM_ARR_TIME = NOM_ARR_TIME + OPD_DATE,
+    ACT_ARR_TIME = ACT_ARR_TIME + OPD_DATE
+  )]
+  invisible(gc(reset = TRUE))
+  return(veh_stoph)
 }
 
 make_partition_sql <- function(table_name, start_year, start_month) {
@@ -51,9 +85,9 @@ make_partition_sql <- function(table_name, start_year, start_month) {
 }
 
 date_convert <- function(trimet_coded_date) {
-  as.Date(lubridate::fast_strptime(
+  lubridate::fast_strptime(
     trimet_coded_date, format = "%d%b%Y:%H:%M:%S", tz = "PST8PDT", lt = FALSE
-  ))
+  )
 }
 
 seconds_of_day_convert <- function(date, seconds_of_day) {
