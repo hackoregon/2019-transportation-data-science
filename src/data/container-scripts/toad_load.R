587a74f58e1ea30b3831e0fb033c301654992c37
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-07-26 07:56:26 -0700

    add stop event reader

--- src/data/container-scripts/toad_load.R
+++ src/data/container-scripts/toad_load.R
@@ -1,26 +1,34 @@
 #! /usr/bin/env Rscript
 
+## Environment variables
+PGHOST <- Sys.getenv("PGHOST")
+PGPORT <- Sys.getenv("PGPORT")
+PGUSER <- Sys.getenv("PGUSER")
+PGPASSWORD <- Sys.getenv("PGPASSWORD")
+PGDATABASE <- Sys.getenv("PGDATABASE")
+WORK_PATH <- Sys.getenv("WORK_PATH")
+
 ## Functions
-connect_toad_database <- function(
-  host, port, user, password, dbname, tries = 5, wait = 5) {
+### PostgreSQL
+connect_toad_database <- function(tries = 5, wait = 5) {
   for (attempt in 1:tries) {
     db_ready <- DBI::dbCanConnect(
       RPostgres::Postgres(),
-      host = host,
-      port = port,
-      user = user,
-      password = password,
-      dbname = dbname
+      host = PGHOST,
+      port = PGPORT,
+      user = PGUSER,
+      password = PGPASSWORD,
+      dbname = PGDATABASE
     )
 
     if (db_ready) { # return connection if it's ready
       return(DBI::dbConnect(
         RPostgres::Postgres(),
-        host = host,
-        port = port,
-        user = user,
-        password = password,
-        dbname = dbname
+        host = PGHOST,
+        port = PGPORT,
+        user = PGUSER,
+        password = PGPASSWORD,
+        dbname = PGDATABASE
       ))
     }
 
@@ -33,14 +41,30 @@ connect_toad_database <- function(
 
 }
 
-load_csv_file <- function(work_path, file_type, month_code) {
+make_partition_sql <- function(table_name, start_year, start_month) {
+  start_date <- lubridate::ymd(sprintf("%4d-%2d-01", start_year, start_month))
+  end_date <- start_date %m+% months(1)
+  sql <- paste0(
+    "CREATE TABLE ", table_name, "_y", start_year, "m", start_month,
+    " PARTITION OF ", table_name,
+    " FOR VALUES FROM ('", start_date, "') TO ('", end_date, "');"
+  )
+  return(sql)
+}
+
+### data.table file input
+load_csv_file <- function(file_type, month_code) {
   filename <- paste0(
-    work_path, "/", file_type, "_", month_code, ".csv")
+    WORK_PATH, "/", file_type, "_", month_code, ".csv")
   return(data.table::fread(file = filename, showProgress = FALSE))
 }
 
-read_tripsh <- function(work_path, month_code) {
-  tripsh <- load_csv_file(work_path, file_type = "raw_tripsh", month_code)
+date_convert <- function(trimet_coded_date) {
+  as.POSIXct(strptime(trimet_coded_date, format = "%d%b%Y:%H:%M:%S", tz = "PST8PDT"))
+}
+
+read_tripsh <- function(month_code) {
+  tripsh <- load_csv_file(file_type = "raw_tripsh", month_code)
   invisible(gc(reset = TRUE))
   tripsh <- tripsh[NOM_END_TIME > 0 & !is.na(ACT_END_TIME) & PATTERN_DIRECTION != ""]
   invisible(gc(reset = TRUE))
@@ -56,8 +80,8 @@ read_tripsh <- function(work_path, month_code) {
   return(tripsh)
 }
 
-read_veh_stoph <- function(work_path, month_code) {
-  veh_stoph <- load_csv_file(work_path, file_type = "raw_veh_stoph", month_code)
+read_veh_stoph <- function(month_code) {
+  veh_stoph <- load_csv_file(file_type = "raw_veh_stoph", month_code)
   invisible(gc(reset = TRUE))
   veh_stoph <- veh_stoph[EVENT_NO_TRIP > 0 & !is.na(NOM_ARR_TIME)]
   invisible(gc(reset = TRUE))
@@ -73,25 +97,20 @@ read_veh_stoph <- function(work_path, month_code) {
   return(veh_stoph)
 }
 
-make_partition_sql <- function(table_name, start_year, start_month) {
-  start_date <- lubridate::ymd(sprintf("%4d-%2d-01", start_year, start_month))
-  end_date <- start_date %m+% months(1)
-  sql <- paste0(
-    "CREATE TABLE ", table_name, "_y", start_year, "m", start_month,
-    " PARTITION OF ", table_name,
-    " FOR VALUES FROM ('", start_date, "') TO ('", end_date, "');"
-  )
-  return(sql)
-}
-
-date_convert <- function(trimet_coded_date) {
-  lubridate::fast_strptime(
-    trimet_coded_date, format = "%d%b%Y:%H:%M:%S", tz = "PST8PDT", lt = FALSE
-  )
-}
-
-seconds_of_day_convert <- function(date, seconds_of_day) {
-  date + seconds_of_day
+read_stop_event <- function(month_code) {
+  stop_event <- load_csv_file(file_type = "raw_stop_event", month_code)
+  invisible(gc(reset = TRUE))
+  #stop_event <- stop_event[EVENT_NO_TRIP > 0 & !is.na(NOM_ARR_TIME)]
+  invisible(gc(reset = TRUE))
+  stop_event <- stop_event[, `:=`(SERVICE_DATE = date_convert(SERVICE_DATE))]
+  invisible(gc(reset = TRUE))
+  stop_event <- stop_event[, `:=`(
+    LEAVE_TIME = LEAVE_TIME + SERVICE_DATE,
+    STOP_TIME = STOP_TIME + SERVICE_DATE,
+    ARRIVE_TIME = ARRIVE_TIME + SERVICE_DATE,
+  )]
+  invisible(gc(reset = TRUE))
+  return(stop_event)
 }
 
 qhod <- function(tstamp) {
