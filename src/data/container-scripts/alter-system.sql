e0069b38c146b3443b82d01457a8030a35f1d5ca
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-07-28 00:00:24 -0700

    values need to be quoted

--- src/data/container-scripts/alter-system.sql
+++ src/data/container-scripts/alter-system.sql
@@ -1,6 +1,5 @@
-ALTER SYSTEM SET shared_buffers = 8017MB;
-ALTER SYSTEM SET work_mem = 2GB;
-ALTER SYSTEM SET maintenance_work_mem = 2GB;
-ALTER SYSTEM SET max_wal_size = 10GB;
-ALTER SYSTEM SET effective_cache_size = 30706MB;
-ALTER SYSTEM SET shared_preload_libraries = 'pg_stat_statements,auto_explain';
+ALTER SYSTEM SET shared_buffers = '8017MB';
+ALTER SYSTEM SET work_mem = '2GB';
+ALTER SYSTEM SET maintenance_work_mem = '2GB';
+ALTER SYSTEM SET max_wal_size = '10GB';
+ALTER SYSTEM SET effective_cache_size = '30706MB';
