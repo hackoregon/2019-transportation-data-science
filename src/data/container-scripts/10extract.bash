44db0a0df7bba1e03052d7856a91a84fd70ac81e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-07-28 11:26:28 -0700

    get rid of intermediate files

--- src/data/container-scripts/10extract.bash
+++ src/data/container-scripts/10extract.bash
@@ -56,6 +56,11 @@ xsv select ${VEH_STOPH_COLS} "init_veh_stoph 1-31OCT2017.csv" > raw_veh_stoph_20
 xsv select ${VEH_STOPH_COLS} "init_veh_stoph 1-30NOV2017.csv" > raw_veh_stoph_2017_11.csv
 xsv select ${VEH_STOPH_COLS} "init_veh_stoph 1-30APR2018.csv" > raw_veh_stoph_2018_04.csv
 xsv select ${VEH_STOPH_COLS} "init_veh_stoph 1-31MAY2018.csv" > raw_veh_stoph_2018_05.csv
+rm "init_veh_stoph 1-30SEP2017.csv"
+rm "init_veh_stoph 1-31OCT2017.csv"
+rm "init_veh_stoph 1-30NOV2017.csv"
+rm "init_veh_stoph 1-30APR2018.csv"
+rm "init_veh_stoph 1-31MAY2018.csv"
 
 echo "old format stop events"
 xsv select ${STOP_EVENT_COLS} "trimet_stop_event 1-30SEP2017.csv" > raw_stop_event_2017_09.csv
@@ -63,6 +68,11 @@ xsv select ${STOP_EVENT_COLS} "trimet_stop_event 1-31OCT2017.csv" > raw_stop_eve
 xsv select ${STOP_EVENT_COLS} "trimet_stop_event 1-30NOV2017.csv" > raw_stop_event_2017_11.csv
 xsv select ${STOP_EVENT_COLS} "trimet_stop_event 1-30APR2018.csv" > raw_stop_event_2018_04.csv
 xsv select ${STOP_EVENT_COLS} "trimet_stop_event 1-31MAY2018.csv" > raw_stop_event_2018_05.csv
+rm "trimet_stop_event 1-30SEP2017.csv"
+rm "trimet_stop_event 1-31OCT2017.csv"
+rm "trimet_stop_event 1-30NOV2017.csv"
+rm "trimet_stop_event 1-30APR2018.csv"
+rm "trimet_stop_event 1-31MAY2018.csv"
 
 echo "old format trips history"
 xsv select ${TRIPSH_COLS} "init_tripsh 1-30SEP2017.csv" > raw_tripsh_2017_09.csv
@@ -70,6 +80,11 @@ xsv select ${TRIPSH_COLS} "init_tripsh 1-31OCT2017.csv" > raw_tripsh_2017_10.csv
 xsv select ${TRIPSH_COLS} "init_tripsh 1-30NOV2017.csv" > raw_tripsh_2017_11.csv
 xsv select ${TRIPSH_COLS} "init_tripsh 1-30APR2018.csv" > raw_tripsh_2018_04.csv
 xsv select ${TRIPSH_COLS} "init_tripsh 1-31MAY2018.csv" > raw_tripsh_2018_05.csv
+rm "init_tripsh 1-30SEP2017.csv"
+rm "init_tripsh 1-31OCT2017.csv"
+rm "init_tripsh 1-30NOV2017.csv"
+rm "init_tripsh 1-30APR2018.csv"
+rm "init_tripsh 1-31MAY2018.csv"
 
 ## The old format files had date stamps of the form "ddMMMyyyy" and the new ones are of the form "ddMMMyy". So we 
 ## standardize on the old format using "sed".
@@ -83,6 +98,9 @@ xsv select ${STOP_EVENT_COLS} "1 stopevent July 2018 to Dec 2018.csv" \
 xsv select ${TRIPSH_COLS} "1 init_tripsh July 2018 to Dec 2018.csv" \
   | sed 's/18:00:00:00/2018:00:00:00/' \
   > new_raw_tripsh.csv
+rm "1 init_veh_stoph July 2018 to Dec 2018.csv"
+rm "1 stopevent July 2018 to Dec 2018.csv"
+rm "1 init_tripsh July 2018 to Dec 2018.csv"
 
 ## The new format files are stacked by data table type with six months of data. We want files for each month.
 echo "extracting new veh_stoph months"
@@ -108,6 +126,8 @@ xsv search "SEP2018" new_raw_tripsh.csv > raw_tripsh_2018_09.csv
 xsv search "OCT2018" new_raw_tripsh.csv > raw_tripsh_2018_10.csv
 xsv search "NOV2018" new_raw_tripsh.csv > raw_tripsh_2018_11.csv
 xsv search "DEC2018" new_raw_tripsh.csv > raw_tripsh_2018_12.csv
+
+rm new_raw_*.csv
 
 popd
 
