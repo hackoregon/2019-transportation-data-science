63b60fc6ec18c194a824cb9ee594b69e4bc0d941
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-02 20:32:15 -0700

    wait for others to finish before rail_passenger_stops

--- src/data/30create_partitioned_tables.bash
+++ src/data/30create_partitioned_tables.bash
@@ -6,7 +6,7 @@ export PGDATABASE=transit_operations_analytics_data
 
 /usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_trips.sql
 /usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_passenger_stops.sql &
-/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=rail_passenger_stops.sql &
 /usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_all_stops.sql &
 wait
+/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=rail_passenger_stops.sql
 /usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=disturbance_stops.sql
