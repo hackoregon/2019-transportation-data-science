ecb00d8f5dd08071d9490796c714c4cc7b92aa7b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-04 01:06:07 -0700

    new columns

--- src/data/rail_passenger_stops.sql
+++ src/data/rail_passenger_stops.sql
@@ -92,6 +92,18 @@ AND route_number IN (SELECT rte FROM rail_routes);
 \echo truncating input table
 TRUNCATE TABLE raw.raw_stop_event;
 
+ALTER TABLE rail_passenger_stops ADD COLUMN IF NOT EXISTS seconds_late integer;
+ALTER TABLE rail_passenger_stops ADD COLUMN IF NOT EXISTS arriving_load integer;
+ALTER TABLE rail_passenger_stops ADD COLUMN IF NOT EXISTS arrive_quarter_hour double precision;
+UPDATE rail_passenger_stops
+SET seconds_late = extract('epoch' from (arrive_time - stop_time)),
+  arriving_load = estimated_load - ons + offs,
+  arrive_quarter_hour = 0.25*trunc(
+	4*date_part('hour', arrive_time AT TIME ZONE 'America/Los_Angeles') +
+	date_part('minute', arrive_time AT TIME ZONE 'America/Los_Angeles')/15
+  )
+;
+
 \echo primary key
 ALTER TABLE rail_passenger_stops 
 ADD PRIMARY KEY (vehicle_id, service_date, arrive_time, leave_time);
