fbe0f5dfead79b53ab8caff8dc27ef2f3689020c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-04 00:53:02 -0700

    ditch extra time calls

--- src/data/27load_raw_data.bash
+++ src/data/27load_raw_data.bash
@@ -5,7 +5,7 @@ export PGUSER=transportation2019
 export PGDATABASE=transit_operations_analytics_data
 
 # load the raw data with SQL
-/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_stop_event.sql &
-/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_tripsh.sql &
-/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_veh_stoph.sql &
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_stop_event.sql &
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_tripsh.sql &
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_veh_stoph.sql &
 wait
--- src/data/90create_database_backup.bash
+++ src/data/90create_database_backup.bash
@@ -5,11 +5,9 @@ export PGUSER=transportation2019
 export PGDATABASE=transit_operations_analytics_data
 export DEST=/Work
 
-#echo "Vacuuming the database"
-#/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --command="VACUUM ANALYZE;"
 echo "Creating the database backup"
 pushd ${DEST}
-/usr/bin/time pg_dump --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${PGDATABASE}.backup
+pg_dump --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${PGDATABASE}.backup
 pg_restore --list ${PGDATABASE}.backup
 sha512sum ${PGDATABASE}.backup > ${PGDATABASE}.backup.sha512sum
 echo "Creating ERD"
