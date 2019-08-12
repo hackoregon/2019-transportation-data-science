cd18fb3d52126e5961e42071121e907dec388b7c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-12 15:43:11 -0700

    add rail table

--- src/data/summary-statistics/summary_statistics.sql
+++ src/data/summary-statistics/summary_statistics.sql
@@ -1,10 +1,11 @@
-CREATE TEMPORARY TABLE bus_passenger_stops_summary AS
+DROP TABLE IF EXISTS bus_passenger_stops_summary;
+CREATE TABLE bus_passenger_stops_summary AS
   SELECT extract('year' from service_date) AS year,
     extract('month' from service_date) AS month,
-    arrive_quarter_hour, count(seconds_late) as samples, 
-    percentile_cont(0.5) within group (order by seconds_late) AS median_seconds_late, 
+    arrive_quarter_hour, count(seconds_late) as samples,
+    percentile_cont(0.5) within group (order by seconds_late) AS median_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
-    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late, 
+    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
     sum(ons) AS system_total_ons, sum(offs) AS system_total_offs
   FROM bus_passenger_stops
   WHERE service_key = 'W'
@@ -13,11 +14,28 @@ CREATE TEMPORARY TABLE bus_passenger_stops_summary AS
 ;
 COPY bus_passenger_stops_summary TO '/Work/bus_passenger_stops_summary.csv' WITH CSV HEADER;
 
-CREATE TEMPORARY TABLE disturbance_stops_summary AS
-  SELECT year, month, start_quarter_hour, count(duration) as samples, 
-    percentile_cont(0.5) within group (order by duration) AS median_duration, 
-    percentile_cont(0.75) within group (order by duration) -
-    percentile_cont(0.25) within group (order by duration) AS iqr_duration
+DROP TABLE IF EXISTS rail_passenger_stops_summary;
+CREATE TABLE rail_passenger_stops_summary AS
+  SELECT extract('year' from service_date) AS year,
+    extract('month' from service_date) AS month,
+    arrive_quarter_hour, count(seconds_late) as samples,
+    percentile_cont(0.5) within group (order by seconds_late) AS median_seconds_late,
+    percentile_cont(0.75) within group (order by seconds_late) -
+    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
+    sum(ons) AS system_total_ons, sum(offs) AS system_total_offs
+  FROM rail_passenger_stops
+  WHERE service_key = 'A'
+  GROUP BY year, month, arrive_quarter_hour
+  ORDER BY year, month, arrive_quarter_hour
+;
+COPY rail_passenger_stops_summary TO '/Work/rail_passenger_stops_summary.csv' WITH CSV HEADER;
+
+DROP TABLE IF EXISTS disturbance_stops_summary;
+CREATE TABLE disturbance_stops_summary AS
+  SELECT year, month, start_quarter_hour, count(duration) as samples,
+    percentile_cont(0.5) within group (order by extract('epoch' from duration)) AS median_duration,
+    percentile_cont(0.75) within group (order by extract('epoch' from duration)) -
+    percentile_cont(0.25) within group (order by extract('epoch' from duration)) AS iqr_duration
   FROM disturbance_stops
   WHERE service_key = 'W'
   GROUP BY year, month, start_quarter_hour
