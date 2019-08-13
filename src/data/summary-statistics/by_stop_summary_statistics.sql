96a7c6fb81d2598fdd2f33994649591ae559f8cd
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-12 20:06:59 -0700

    quantiles of ons and offs aren't meaningful

--- src/data/summary-statistics/by_stop_summary_statistics.sql
+++ src/data/summary-statistics/by_stop_summary_statistics.sql
@@ -8,16 +8,6 @@ CREATE TABLE bus_passenger_by_stop_summary AS
     percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
     percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
-    percentile_cont(0.05) within group (order by ons) AS p05_ons,
-    percentile_cont(0.5) within group (order by ons) AS median_ons,
-    percentile_cont(0.95) within group (order by ons) AS p95_ons,
-    percentile_cont(0.75) within group (order by ons) -
-    percentile_cont(0.25) within group (order by ons) AS iqr_ons,
-    percentile_cont(0.05) within group (order by offs) AS p05_offs,
-    percentile_cont(0.5) within group (order by offs) AS median_offs,
-    percentile_cont(0.95) within group (order by offs) AS p95_offs,
-    percentile_cont(0.75) within group (order by offs) -
-    percentile_cont(0.25) within group (order by offs) AS iqr_offs,
     sum(ons) AS system_total_ons, sum(offs) AS system_total_offs
   FROM bus_passenger_stops
   WHERE service_key = 'W'
@@ -35,16 +25,6 @@ CREATE TABLE rail_passenger_by_stop_summary AS
     percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
     percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
-    percentile_cont(0.05) within group (order by ons) AS p05_ons,
-    percentile_cont(0.5) within group (order by ons) AS median_ons,
-    percentile_cont(0.95) within group (order by ons) AS p95_ons,
-    percentile_cont(0.75) within group (order by ons) -
-    percentile_cont(0.25) within group (order by ons) AS iqr_ons,
-    percentile_cont(0.05) within group (order by offs) AS p05_offs,
-    percentile_cont(0.5) within group (order by offs) AS median_offs,
-    percentile_cont(0.95) within group (order by offs) AS p95_offs,
-    percentile_cont(0.75) within group (order by offs) -
-    percentile_cont(0.25) within group (order by offs) AS iqr_offs,
     sum(ons) AS system_total_ons, sum(offs) AS system_total_offs
   FROM rail_passenger_stops
   WHERE service_key = 'A'
