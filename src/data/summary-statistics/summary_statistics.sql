c55e8885bc8f97a659b1417f5b4f8d247b67e603
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-12 20:14:10 -0700

    more quantiles

--- src/data/summary-statistics/summary_statistics.sql
+++ src/data/summary-statistics/summary_statistics.sql
@@ -3,10 +3,14 @@ CREATE TABLE bus_passenger_stops_summary AS
   SELECT extract('year' from service_date) AS year,
     extract('month' from service_date) AS month,
     arrive_quarter_hour, count(seconds_late) as samples,
-    percentile_cont(0.5) within group (order by seconds_late) AS median_seconds_late,
+    percentile_cont(0.05) within group (order by seconds_late) AS p05_seconds_late,
+    percentile_cont(0.25) within group (order by seconds_late) AS q1_seconds_late,
+    percentile_cont(0.50) within group (order by seconds_late) AS median_seconds_late,
+    percentile_cont(0.75) within group (order by seconds_late) AS q3_seconds_late,
+    percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
     percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
-    sum(ons) AS system_total_ons, sum(offs) AS system_total_offs
+    sum(ons) AS monthly_total_ons, sum(offs) AS monthly_total_offs
   FROM bus_passenger_stops
   WHERE service_key = 'W'
   GROUP BY year, month, arrive_quarter_hour
@@ -18,10 +22,14 @@ CREATE TABLE rail_passenger_stops_summary AS
   SELECT extract('year' from service_date) AS year,
     extract('month' from service_date) AS month,
     arrive_quarter_hour, count(seconds_late) as samples,
-    percentile_cont(0.5) within group (order by seconds_late) AS median_seconds_late,
+    percentile_cont(0.05) within group (order by seconds_late) AS p05_seconds_late,
+    percentile_cont(0.25) within group (order by seconds_late) AS q1_seconds_late,
+    percentile_cont(0.50) within group (order by seconds_late) AS median_seconds_late,
+    percentile_cont(0.75) within group (order by seconds_late) AS q3_seconds_late,
+    percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
     percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
-    sum(ons) AS system_total_ons, sum(offs) AS system_total_offs
+    sum(ons) AS monthly_total_ons, sum(offs) AS monthly_total_offs
   FROM rail_passenger_stops
   WHERE service_key = 'A'
   GROUP BY year, month, arrive_quarter_hour
@@ -31,7 +39,11 @@ CREATE TABLE rail_passenger_stops_summary AS
 DROP TABLE IF EXISTS disturbance_stops_summary;
 CREATE TABLE disturbance_stops_summary AS
   SELECT year, month, start_quarter_hour, count(duration) as samples,
-    percentile_cont(0.5) within group (order by extract('epoch' from duration)) AS median_duration,
+    percentile_cont(0.05) within group (order by extract('epoch' from duration)) AS p05_duration,
+    percentile_cont(0.25) within group (order by extract('epoch' from duration)) AS q1_duration,
+    percentile_cont(0.50) within group (order by extract('epoch' from duration)) AS median_duration,
+    percentile_cont(0.75) within group (order by extract('epoch' from duration)) AS q3_duration,
+    percentile_cont(0.95) within group (order by extract('epoch' from duration)) AS p95_duration,
     percentile_cont(0.75) within group (order by extract('epoch' from duration)) -
     percentile_cont(0.25) within group (order by extract('epoch' from duration)) AS iqr_duration
   FROM disturbance_stops
