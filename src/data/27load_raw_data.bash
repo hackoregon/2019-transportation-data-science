49bacfd917fa4e8ad18e0891276b4ce6042c93b7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-15 20:22:37 -0700

    take a backup after loading raw data

--- src/data/27load_raw_data.bash
+++ src/data/27load_raw_data.bash
@@ -3,9 +3,17 @@
 # define parameters
 export PGUSER=transportation2019
 export PGDATABASE=transit_operations_analytics_data
+export DEST=/Work
 
 # load the raw data with SQL
 psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_stop_event.sql &
 psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_tripsh.sql &
 psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_veh_stoph.sql &
 wait
+
+echo "Creating raw database backup"
+pushd ${DEST}
+pg_dump --verbose --format=c --dbname=${PGDATABASE} > ${PGDATABASE}_raw.backup
+pg_restore --list ${PGDATABASE}_raw.backup
+sha512sum ${PGDATABASE}_raw.backup > ${PGDATABASE}_raw.backup.sha512sum
+popd
