d0055a350f216e7067a49c34ab8f256800592acf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-16 17:21:45 -0700

    add count of stops

--- src/data/passenger_stops_trips.sql
+++ src/data/passenger_stops_trips.sql
@@ -1,7 +1,9 @@
+SET timezone = 'PST8PDT';
 DROP TABLE IF EXISTS passenger_stops_trips CASCADE;
 CREATE TABLE passenger_stops_trips AS
-SELECT DISTINCT vehicle_number, train, trip_number, date_stamp::date AS service_date, service_key,
-  route_number, direction
+SELECT DISTINCT date_stamp::date AS service_date,
+    vehicle_number, train, route_number, direction, service_key, trip_number,
+	COUNT(location_id) AS stop_count
 FROM raw.raw_stop_event
 WHERE vehicle_number > 0
 AND train > 0
@@ -9,6 +11,8 @@ AND trip_number > 0
 AND service_key IS NOT NULL
 AND route_number > 0
 AND route_number <= 291
+GROUP BY date_stamp::date,
+    vehicle_number, train, route_number, direction, service_key, trip_number
 ;
 ALTER TABLE passenger_stops_trips ADD COLUMN passenger_stops_trips_key serial;
 ALTER TABLE passenger_stops_trips ADD PRIMARY KEY (passenger_stops_trips_key);
