cf0f77ebadcbe509669286c7a587ae87b5ac14ee
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-17 01:59:15 -0700

    sort the table

--- src/data/passenger_stops_trips.sql
+++ src/data/passenger_stops_trips.sql
@@ -1,7 +1,7 @@
 SET timezone = 'PST8PDT';
 DROP TABLE IF EXISTS passenger_stops_trips CASCADE;
 CREATE TABLE passenger_stops_trips AS
-SELECT DISTINCT date_stamp::date AS service_date,
+SELECT DISTINCT date_stamp AS service_date,
     vehicle_number, train, route_number, direction, service_key, trip_number,
 	COUNT(location_id) AS stop_count
 FROM raw.raw_stop_event
@@ -11,8 +11,9 @@ AND trip_number > 0
 AND service_key IS NOT NULL
 AND route_number > 0
 AND route_number <= 291
-GROUP BY date_stamp::date,
+GROUP BY date_stamp,
     vehicle_number, train, route_number, direction, service_key, trip_number
+ORDER BY vehicle_number, service_date, trip_number
 ;
 ALTER TABLE passenger_stops_trips ADD COLUMN passenger_stops_trips_key serial;
 ALTER TABLE passenger_stops_trips ADD PRIMARY KEY (passenger_stops_trips_key);
