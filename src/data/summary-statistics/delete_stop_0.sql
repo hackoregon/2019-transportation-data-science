fecab9f8513ac45c38881c74d2b473961cad55ae
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-19 11:01:08 -0700

    not needed - done in build now

--- src/data/summary-statistics/delete_stop_0.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM bus_passenger_stops
-WHERE location_id = 0;
-DELETE FROM rail_passenger_stops
-WHERE location_id = 0;
--- src/data/summary-statistics/floor_seconds_late.sql
+++
@@ -1,7 +0,0 @@
-UPDATE bus_passenger_stops
-SET seconds_late = 0
-WHERE seconds_late < 0
-;
-UPDATE rail_passenger_stops
-SET seconds_late = 0
-WHERE seconds_late < 0;
