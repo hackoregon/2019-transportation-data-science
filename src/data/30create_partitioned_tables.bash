8cf74dd832d2f34db76b55a264ec56f5e5aa7637
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-19 12:06:42 -0700

    all open issues except stop-to-stop

--- src/data/30create_partitioned_tables.bash
+++ src/data/30create_partitioned_tables.bash
@@ -4,8 +4,15 @@
 export PGUSER=transportation2019
 export PGDATABASE=transit_operations_analytics_data
 
-psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_trips.sql
-psql --username=${PGUSER} --dbname=${PGDATABASE} --file=rail_passenger_stops.sql
-psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_passenger_stops.sql
-psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_all_stops.sql
-psql --username=${PGUSER} --dbname=${PGDATABASE} --file=disturbance_stops.sql
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_trips.sql &
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=rail_passenger_stops.sql &
+wait
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_all_stops.sql &
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=bus_passenger_stops.sql &
+wait
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=disturbance_stops.sql &
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=summary-statistics/by_stop_summary.sql &
+wait
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=summary-statistics/rush_hour_summaries.sql &
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=summary-statistics/system_wide_summary.sql &
+wait
