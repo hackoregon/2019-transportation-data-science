7b1a3358905a60ba479f4cf696b9ba4666444e38
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-21 01:11:25 -0700

    don't vacuum

--- src/data/90create_database_backup.bash
+++ src/data/90create_database_backup.bash
@@ -8,7 +8,6 @@ export DEST=/Work
 echo "Cleaning up database"
 psql --username=${PGUSER} --dbname=${PGDATABASE} --command="DROP SCHEMA IF EXISTS raw CASCADE;"
 psql --username=${PGUSER} --dbname=${PGDATABASE} --command="DROP TABLE IF EXISTS bus_all_stops CASCADE;"
-/usr/bin/time psql --username=${PGUSER} --dbname=${PGDATABASE} --command="VACUUM ANALYZE;"
 echo "Creating the database backup"
 pushd ${DEST}
 pg_dump --verbose --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${PGDATABASE}.backup
