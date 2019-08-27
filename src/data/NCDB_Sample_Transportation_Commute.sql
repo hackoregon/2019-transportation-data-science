a70b0f860a46682e7e074a3ac40077daaa6cf40e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-26 23:23:48 -0700

    NCDB_Sample_Transportation_Commute table

---
+++ src/data/NCDB_Sample_Transportation_Commute.sql
@@ -0,0 +1,41 @@
+SET timezone = 'PST8PDT';
+DROP TABLE IF EXISTS ncdb_sample_transportation_commute CASCADE;
+CREATE TABLE ncdb_sample_transportation_commute (
+  tract_geo_fips text,
+  t128_001_17 integer,
+  t128_002_17 integer,
+  t128_009_17 integer,
+  t128_010_17 integer,
+  t128_003_17 integer,
+  t128_004_17 integer,
+  t128_005_17 integer,
+  t128_006_17 integer,
+  t128_007_17 integer,
+  t128_008_17 integer,
+  t129_001_17 integer,
+  t129_002_17 integer,
+  t129_003_17 integer,
+  t129_004_17 integer,
+  t129_005_17 integer,
+  t129_006_17 integer,
+  t129_007_17 integer,
+  t129_008_17 integer,
+  t129_009_17 integer,
+  t129_010_17 integer,
+  t147_001_17 integer,
+  opp_zone boolean,
+  opp_zone_elig text
+);
+COPY ncdb_sample_transportation_commute FROM '/Work/NCDB_Sample_Transportation_Commute.csv' WITH csv header;
+
+-- remove tracts not in Portland - Vancouver - Hillsboro CBSA
+DELETE FROM ncdb_sample_transportation_commute
+  WHERE tract_geo_fips NOT LIKE '41005%' -- Clackamas
+  AND tract_geo_fips NOT LIKE '41009%' -- Columbia
+  AND tract_geo_fips NOT LIKE '41051%' -- Multnomah
+  AND tract_geo_fips NOT LIKE '41067%' -- Washington
+  AND tract_geo_fips NOT LIKE '41071%' -- Yamhill
+  AND tract_geo_fips NOT LIKE '53011%' -- Clark
+  AND tract_geo_fips NOT LIKE '53059%' -- Skamania
+;
+ALTER TABLE ncdb_sample_transportation_commute ADD PRIMARY KEY (tract_geo_fips);
