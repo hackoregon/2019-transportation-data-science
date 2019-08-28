72667339af1955c0cddbcebb982f0f5a94a887c5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-28 12:35:54 -0700

    load all the raw data - don't edit

--- src/data/27load_raw_data.bash
+++ src/data/27load_raw_data.bash
@@ -6,9 +6,10 @@ export PGDATABASE=transit_operations_analytics_data
 export DEST=/Work
 
 # load the raw data with SQL
-psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_stop_event.sql
 psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_tripsh.sql
-psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_veh_stoph.sql
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_stop_event.sql &
+psql --username=${PGUSER} --dbname=${PGDATABASE} --file=load_raw_veh_stoph.sql &
+wait
 
 echo "Creating raw database backup"
 pushd ${DEST}
