9c31a60b9f2f2dfac8483c411bacc787cd76392b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-29 12:43:22 -0700

    need to load reference before NCDB table

--- src/data/00runall.bash
+++ src/data/00runall.bash
@@ -4,8 +4,8 @@
 /usr/bin/time ./15create_database.bash
 /usr/bin/time ./20traffic_signals.bash
 /usr/bin/time ./22passenger_census.bash
-/usr/bin/time ./23NCDB_Sample_Transportation_Commute.bash
 /usr/bin/time ./24load_reference.bash
+/usr/bin/time ./25NCDB_Sample_Transportation_Commute.bash
 /usr/bin/time ./27load_raw_data.bash
 /usr/bin/time ./30create_partitioned_tables.bash
 /usr/bin/time ./70create_database_backup.bash
