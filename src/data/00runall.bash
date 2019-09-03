576a1363254e995441f78237ab5285172426d7e5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-09-02 18:36:28 -0700

    do reference earlier!

--- src/data/00runall.bash
+++ src/data/00runall.bash
@@ -3,8 +3,8 @@
 /usr/bin/time ./10extract.bash
 /usr/bin/time ./15create_database.bash
 /usr/bin/time ./20traffic_signals.bash
+/usr/bin/time ./21load_reference.bash
 /usr/bin/time ./22passenger_census.bash
-/usr/bin/time ./24load_reference.bash
 /usr/bin/time ./25NCDB_Sample_Transportation_Commute.bash
 /usr/bin/time ./27load_raw_data.bash
 /usr/bin/time ./30create_partitioned_tables.bash
