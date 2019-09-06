45753faafdc204a852040544a545be86c1ca063a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-09-06 00:48:23 -0700

    don't drop anything before backup

--- src/data/70create_database_backup.bash
+++ src/data/70create_database_backup.bash
@@ -5,9 +5,6 @@ export PGUSER=transportation2019
 export PGDATABASE=transit_operations_analytics_data
 export DEST=/Work
 
-echo "Cleaning up database"
-psql --username=${PGUSER} --dbname=${PGDATABASE} --command="DROP SCHEMA IF EXISTS raw CASCADE;"
-psql --username=${PGUSER} --dbname=${PGDATABASE} --command="DROP TABLE IF EXISTS bus_all_stops CASCADE;"
 echo "Creating the database backup"
 pushd ${DEST}
 pg_dump --verbose --format=c --exclude-schema=raw --dbname=${PGDATABASE} > ${PGDATABASE}.backup
