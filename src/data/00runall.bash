383f639c6ddb2fd4611ca6adea1a687c4b57740e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-09-06 11:57:44 -0700

    reordering

--- src/data/00runall.bash
+++ src/data/00runall.bash
@@ -8,5 +8,8 @@
 /usr/bin/time ./25NCDB_Sample_Transportation_Commute.bash
 /usr/bin/time ./26busstop_catchment_zone_with_census_attribs.bash
 /usr/bin/time ./27load_raw_data.bash
-/usr/bin/time ./30create_partitioned_tables.bash
+/usr/bin/time ./30rail_passenger_stops.bash
+/usr/bin/time ./32bus_passenger_stops.bash
+/usr/bin/time ./34disturbance_stops.bash
+/usr/bin/time ./36summary_statistics.bash
 /usr/bin/time ./70create_database_backup.bash
