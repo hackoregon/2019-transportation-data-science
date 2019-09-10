8805e440aa2109603017463101a96dc115fd1095
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-09-10 00:53:05 -0700

    comments

--- src/data/22passenger_census.bash
+++ src/data/22passenger_census.bash
@@ -5,6 +5,9 @@ export PGUSER=transportation2019
 export PGDATABASE=transit_operations_analytics_data
 
 # load the raw data with SQL
+echo "passenger_census.sql"
 psql --username=${PGUSER} --dbname=${PGDATABASE} --file=passenger_census.sql
+echo "master_stop_names.sql"
 psql --username=${PGUSER} --dbname=${PGDATABASE} --file=master_stop_names.sql
+echo "ridership_change_tables.sql"
 psql --username=${PGUSER} --dbname=${PGDATABASE} --file=ridership_change_tables.sql
