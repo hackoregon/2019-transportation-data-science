4cbb167542ecab30001b762f796ec521d23ace4e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-09-11 00:13:20 -0700

    add stop names

--- src/data/summary-statistics/by_stop_summary.sql
+++ src/data/summary-statistics/by_stop_summary.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS bus_by_stop_summary;
 CREATE TABLE bus_by_stop_summary AS
-  SELECT location_id, route_number, direction, passenger_stop_locations.longitude,
+  SELECT location_id, stop_name, route_number, direction, passenger_stop_locations.longitude,
     passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326,
     arrive_quarter_hour, count(seconds_late) as samples,
     percentile_cont(0.05) within group (order by seconds_late) AS p05_seconds_late,
@@ -9,12 +9,12 @@ CREATE TABLE bus_by_stop_summary AS
     percentile_cont(0.75) within group (order by seconds_late) AS q3_seconds_late,
     percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
-    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
+      percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
     sum(ons) AS total_ons, sum(offs) AS total_offs
   FROM bus_passenger_stops
   INNER JOIN passenger_stop_locations ON (location_id = stop_id)
   WHERE service_key = 'W'
-  GROUP BY location_id, route_number, direction, arrive_quarter_hour,
+  GROUP BY location_id, stop_name, route_number, direction, arrive_quarter_hour,
     passenger_stop_locations.longitude, passenger_stop_locations.latitude,
     passenger_stop_locations.geom_point_4326
   ORDER BY location_id, route_number, direction, arrive_quarter_hour
@@ -24,7 +24,7 @@ ALTER TABLE bus_by_stop_summary ADD PRIMARY KEY (id);
 
 DROP TABLE IF EXISTS rail_by_stop_summary;
 CREATE TABLE rail_by_stop_summary AS
-  SELECT location_id, route_number, direction, passenger_stop_locations.longitude,
+  SELECT location_id, stop_name, route_number, direction, passenger_stop_locations.longitude,
     passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326,
     arrive_quarter_hour, count(seconds_late) as samples,
     percentile_cont(0.05) within group (order by seconds_late) AS p05_seconds_late,
@@ -33,12 +33,12 @@ CREATE TABLE rail_by_stop_summary AS
     percentile_cont(0.75) within group (order by seconds_late) AS q3_seconds_late,
     percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
-    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
+      percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
     sum(ons) AS total_ons, sum(offs) AS total_offs
   FROM rail_passenger_stops
   INNER JOIN passenger_stop_locations ON (location_id = stop_id)
   WHERE service_key = 'A'
-  GROUP BY location_id, route_number, direction, arrive_quarter_hour,
+  GROUP BY location_id, stop_name, route_number, direction, arrive_quarter_hour,
     passenger_stop_locations.longitude, passenger_stop_locations.latitude,
     passenger_stop_locations.geom_point_4326
   ORDER BY location_id, route_number, direction, arrive_quarter_hour
--- src/data/summary-statistics/rush_hour_summaries.sql
+++ src/data/summary-statistics/rush_hour_summaries.sql
@@ -1,22 +1,23 @@
 DROP TABLE IF EXISTS bus_am_rush_summary;
 CREATE TABLE bus_am_rush_summary AS
-  SELECT location_id, route_number, direction, passenger_stop_locations.longitude,
-    passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326,
-    count(seconds_late) as samples,
+  SELECT location_id, stop_name, route_number, direction, 
+    passenger_stop_locations.longitude, passenger_stop_locations.latitude,
+    passenger_stop_locations.geom_point_4326, count(seconds_late) as samples,
     percentile_cont(0.05) within group (order by seconds_late) AS p05_seconds_late,
     percentile_cont(0.25) within group (order by seconds_late) AS q1_seconds_late,
     percentile_cont(0.50) within group (order by seconds_late) AS median_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) AS q3_seconds_late,
     percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
-    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
+      percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
     sum(ons) AS total_ons, sum(offs) AS total_offs
   FROM bus_passenger_stops
   INNER JOIN passenger_stop_locations ON (location_id = stop_id)
   WHERE service_key = 'W'
   AND arrive_quarter_hour BETWEEN 6.5 AND 8.75
-  GROUP BY location_id, route_number, direction, passenger_stop_locations.longitude,
-    passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326
+  GROUP BY location_id, stop_name, route_number, direction,
+    passenger_stop_locations.longitude, passenger_stop_locations.latitude,
+    passenger_stop_locations.geom_point_4326
   ORDER BY location_id, route_number, direction
 ;
 ALTER TABLE bus_am_rush_summary ADD COLUMN id serial;
@@ -24,23 +25,24 @@ ALTER TABLE bus_am_rush_summary ADD PRIMARY KEY (id);
 
 DROP TABLE IF EXISTS rail_am_rush_summary;
 CREATE TABLE rail_am_rush_summary AS
-  SELECT location_id, route_number, direction, passenger_stop_locations.longitude,
-    passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326,
-    count(seconds_late) as samples,
+  SELECT location_id, stop_name, route_number, direction, 
+    passenger_stop_locations.longitude, passenger_stop_locations.latitude,
+    passenger_stop_locations.geom_point_4326, count(seconds_late) as samples,
     percentile_cont(0.05) within group (order by seconds_late) AS p05_seconds_late,
     percentile_cont(0.25) within group (order by seconds_late) AS q1_seconds_late,
     percentile_cont(0.50) within group (order by seconds_late) AS median_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) AS q3_seconds_late,
     percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
-    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
+      percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
     sum(ons) AS total_ons, sum(offs) AS total_offs
   FROM rail_passenger_stops
   INNER JOIN passenger_stop_locations ON (location_id = stop_id)
   WHERE service_key = 'A'
   AND arrive_quarter_hour BETWEEN 6.5 AND 8.75
-  GROUP BY location_id, route_number, direction, passenger_stop_locations.longitude,
-    passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326
+  GROUP BY location_id, stop_name, route_number, direction,
+    passenger_stop_locations.longitude, passenger_stop_locations.latitude,
+    passenger_stop_locations.geom_point_4326
   ORDER BY location_id, route_number, direction
 ;
 ALTER TABLE rail_am_rush_summary ADD COLUMN id serial;
@@ -48,23 +50,24 @@ ALTER TABLE rail_am_rush_summary ADD PRIMARY KEY (id);
 
 DROP TABLE IF EXISTS bus_pm_rush_summary;
 CREATE TABLE bus_pm_rush_summary AS
-  SELECT location_id, route_number, direction, passenger_stop_locations.longitude,
-    passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326,
-    count(seconds_late) as samples,
+  SELECT location_id, stop_name, route_number, direction, 
+    passenger_stop_locations.longitude, passenger_stop_locations.latitude,
+    passenger_stop_locations.geom_point_4326, count(seconds_late) as samples,
     percentile_cont(0.05) within group (order by seconds_late) AS p05_seconds_late,
     percentile_cont(0.25) within group (order by seconds_late) AS q1_seconds_late,
     percentile_cont(0.50) within group (order by seconds_late) AS median_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) AS q3_seconds_late,
     percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
-    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
+      percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
     sum(ons) AS total_ons, sum(offs) AS total_offs
   FROM bus_passenger_stops
   INNER JOIN passenger_stop_locations ON (location_id = stop_id)
   WHERE service_key = 'W'
   AND arrive_quarter_hour BETWEEN 14.5 AND 18.5
-  GROUP BY location_id, route_number, direction, passenger_stop_locations.longitude,
-    passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326
+  GROUP BY location_id, stop_name, route_number, direction,
+    passenger_stop_locations.longitude, passenger_stop_locations.latitude,
+    passenger_stop_locations.geom_point_4326
   ORDER BY location_id, route_number, direction
 ;
 ALTER TABLE bus_pm_rush_summary ADD COLUMN id serial;
@@ -72,23 +75,24 @@ ALTER TABLE bus_pm_rush_summary ADD PRIMARY KEY (id);
 
 DROP TABLE IF EXISTS rail_pm_rush_summary;
 CREATE TABLE rail_pm_rush_summary AS
-  SELECT location_id, route_number, direction, passenger_stop_locations.longitude,
-    passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326,
-    count(seconds_late) as samples,
+  SELECT location_id, stop_name, route_number, direction, 
+    passenger_stop_locations.longitude, passenger_stop_locations.latitude,
+    passenger_stop_locations.geom_point_4326, count(seconds_late) as samples,
     percentile_cont(0.05) within group (order by seconds_late) AS p05_seconds_late,
     percentile_cont(0.25) within group (order by seconds_late) AS q1_seconds_late,
     percentile_cont(0.50) within group (order by seconds_late) AS median_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) AS q3_seconds_late,
     percentile_cont(0.95) within group (order by seconds_late) AS p95_seconds_late,
     percentile_cont(0.75) within group (order by seconds_late) -
-    percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
+      percentile_cont(0.25) within group (order by seconds_late) AS iqr_seconds_late,
     sum(ons) AS total_ons, sum(offs) AS total_offs
   FROM rail_passenger_stops
   INNER JOIN passenger_stop_locations ON (location_id = stop_id)
   WHERE service_key = 'A'
   AND arrive_quarter_hour BETWEEN 14.5 AND 18.5
-  GROUP BY location_id, route_number, direction, passenger_stop_locations.longitude,
-    passenger_stop_locations.latitude, passenger_stop_locations.geom_point_4326
+  GROUP BY location_id, stop_name, route_number, direction,
+    passenger_stop_locations.longitude, passenger_stop_locations.latitude,
+    passenger_stop_locations.geom_point_4326
   ORDER BY location_id, route_number, direction
 ;
 ALTER TABLE rail_pm_rush_summary ADD COLUMN id serial;
